ae4d80b65fc85a923b1a92d15972de777989d650
sunghogo <leinad5592@gmail.com>
2023-03-22 17:36:39 -0400

    TCJC2023: Section 2: L12 Data Types: Data Types Assignment

--- The Complete Javascript Course 2023/Assignments/assignments.js
+++ The Complete Javascript Course 2023/Assignments/assignments.js
@@ -1,7 +1,20 @@
+// Lecture 10 Values and Variables
 let country = "United States";
 let continent = "North America";
 let population = "331.9";
 
+/*
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+
+//Lecture 12 Data Types
+let isIsland = false;
+let language;
+
+console.log(isIsland);
 console.log(country);
 console.log(continent);
 console.log(population);
