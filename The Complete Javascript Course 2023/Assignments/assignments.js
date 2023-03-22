7e013ab8488ba5392179795877a52f9557ce791a
sunghogo <leinad5592@gmail.com>
2023-03-22 19:56:52 -0400

    TCJC2023: Section 2: L13 let, const and var: Assignment

--- The Complete Javascript Course 2023/Assignments/assignments.js
+++ The Complete Javascript Course 2023/Assignments/assignments.js
@@ -1,9 +1,9 @@
 // Lecture 10 Values and Variables
+/*
 let country = "United States";
 let continent = "North America";
 let population = "331.9";
 
-/*
 console.log(country);
 console.log(continent);
 console.log(population);
@@ -11,10 +11,21 @@ console.log(population);
 
 
 //Lecture 12 Data Types
+/*
 let isIsland = false;
 let language;
 
 console.log(isIsland);
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(population);\
+*/
+
+// Lecture 13 let, const and var
+language = "English";
+const country = "United States";
+const continent = "North America";
+let population = "331.9";
+const isIsland = false;
+// isIsland = true;
+
