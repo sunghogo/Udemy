985386f2ec404344b3c5423674546caf3c59894f
sunghogo <leinad5592@gmail.com>
2023-03-22 18:23:33 -0400

    TCJC2023: Section 2: L13 let, const, var: Starter Code: JS let, const, var sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,7 @@
 /*
+// L6-10
 let js = 'amazing';
-// console.log(40 + 8 + 23 -10);
+console.log(40 + 8 + 23 -10);
 
 console.log("Jonas");
 console.log(23);
@@ -27,8 +28,8 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
-*/
 
+// L!2 Data Types
 let javascriptIsFun= true;
 console.log(true);
 
@@ -48,3 +49,18 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+
+// L13 let, const, var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
