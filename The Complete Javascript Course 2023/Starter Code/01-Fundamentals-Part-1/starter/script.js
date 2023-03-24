62008205d392100cfaffbe3088b26d07ba8de037
sunghogo <leinad5592@gmail.com>
2023-03-23 23:15:40 -0400

    TCJC2023: Section 2: L14 Basic Operators: Starter Code: Operator sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -49,7 +49,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 // L13 let, const, var
 let age = 30;
@@ -64,3 +63,36 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
+*/
+
+// L14 Basic Operators
+
+// Math operators
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtman";
+console.log(firstName + " " + lastName);
+
+// Assignment oerators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=< <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
