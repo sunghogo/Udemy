51726fd3bfde5dd0e2b8a6e6e337d2b05cb6677f
sunghogo <leinad5592@gmail.com>
2023-03-23 23:25:22 -0400

    TCJC2023: Section 2: L14 Basic Operators: Assignment

--- The Complete Javascript Course 2023/Assignments/assignments.js
+++ The Complete Javascript Course 2023/Assignments/assignments.js
@@ -22,10 +22,25 @@ console.log(population);\
 */
 
 // Lecture 13 let, const and var
-language = "English";
+const language = "English";
 const country = "United States";
 const continent = "North America";
 let population = "331.9";
 const isIsland = false;
 // isIsland = true;
 
+// Lecture 14: Basic Operators
+const halfPopulation = population / 2;
+population++;
+console.log(population);
+
+const finlandPopulation = 6;
+const populationGreaterFinland = population > finlandPopulation;
+console.log(populationGreaterFinland);
+
+const averagePopulation = 33;
+const populationGreaterAverage = population < averagePopulation;
+console.log(populationGreaterAverage);
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language; 
+console.log(description);
