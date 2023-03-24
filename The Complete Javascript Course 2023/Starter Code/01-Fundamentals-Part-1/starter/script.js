8bd6df4b134178789d58b94abe7dd5b442fe802f
sunghogo <leinad5592@gmail.com>
2023-03-23 23:37:55 -0400

    TCJC2023: Section 2: L15 Operator Precedence: Starter Code: Operator precdence sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -63,10 +63,9 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
-*/
+
 
 // L14 Basic Operators
-
 // Math operators
 const now = 2037
 const ageJonas = now - 1991;
@@ -96,3 +95,18 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+*/
+
+// L15 Operator Precedence
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah);
