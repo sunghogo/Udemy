c1314fff70f033b0714b213e05f97cfda134ca67
sunghogo <leinad5592@gmail.com>
2023-03-25 23:37:22 -0400

    TCJC2023: Section 2: L18 Taking Decision if / else Statements: Starter Code: if / else sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -109,7 +109,6 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2
 console.log(ageJonas, ageSarah);
-*/
 
 // L17 Strings and Template Literals
 
@@ -133,3 +132,25 @@ lines");
 console.log(`String
 multiple
 lines`);
+*/
+
+// L18 Taking Decisions if / else Statements
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license 🚗')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
