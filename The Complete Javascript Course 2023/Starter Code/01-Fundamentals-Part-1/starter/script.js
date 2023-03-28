8e7ef6613bb68f1b8f5e6c6d80dd5128620abbbd
sunghogo <leinad5592@gmail.com>
2023-03-27 23:29:33 -0400

    TCJC2023: Section 2: L20 Type Covnersion and Coercion: Starter Code: Type conversion/coercion sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -156,3 +156,29 @@ console.log(century);
 */
 
 // L20 Type Conversion and Coercion
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' / '2');
+console.log('23' ** '2');
+
+
+let n = '1' + 1; // '11'
+n = n - 1; // 11 - 1 = 10
+console.log(n);
+
+console.log(2 + 3 + 4 + '5'); // '95'
+console.log('10' - '4' - '3' - 2  + '5') // '15'
