04d0db5bdec5eddf5f9265d4ab6dfc1c2f968082
sunghogo <leinad5592@gmail.com>
2023-03-27 23:34:05 -0400

    TCJC2023: Section 2: L20 Type Covnersion and Coercion: Assignment

--- The Complete Javascript Course 2023/Assignments/assignments.js
+++ The Complete Javascript Course 2023/Assignments/assignments.js
@@ -57,3 +57,10 @@ if (population > 33) {
 } else {
     console.log(`${country}'s population is ${33 - population} below average`);
 }
+
+// L20 Type Conversion and Coercion
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // '617'
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
