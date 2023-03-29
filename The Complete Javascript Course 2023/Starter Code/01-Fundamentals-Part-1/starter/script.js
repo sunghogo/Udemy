a90214e0d53654ebbb68192b776816e7edcf840e
sunghogo <leinad5592@gmail.com>
2023-03-28 23:50:29 -0400

    TCJC2023: Section 2: L22 Equality Operators: == vs. ===: Starter Code: Equality and nonequality sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -222,3 +222,20 @@ if (age == 18) console.log('You just became an adult :D (loose)');
 
 console.log('18' == 18);
 console.log('18' === 18);
+
+const favourite = Number(prompt("What's your favorite numebr?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+// if (favourite == 23) { // '23' == 23
+if (favourite === 23) { // 23 === 23
+    console.log('Cool! 23 is an amazing number!');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number!');
+} else if (favourite === 9) {
+    console.log('9 is also a cool number!');
+} else {
+    console.log('Number is not 23 or 7');
+}
+
+if (favourite !== 23) console.log('Why not 23?'); 
