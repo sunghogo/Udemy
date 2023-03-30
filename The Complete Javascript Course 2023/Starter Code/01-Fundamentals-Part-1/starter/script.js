eeae9755a4af54ced4e14bc5ad034226aed206d8
sunghogo <leinad5592@gmail.com>
2023-03-30 10:58:37 -0400

    TCJC2023: Section 2: L26 The switch Statement: Starter Code: switch case vs conditional sandboxing

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -290,7 +290,6 @@ switch(day) {
         console.log('Prepare theory videos');
         break
     case 'wednesday':
-        break;
     case 'thursday':
         console.log('Write code examples');
         break;
@@ -298,11 +297,24 @@ switch(day) {
         console.log('Record videos');
         break;
     case 'saturday':
-        break;
     case 'sunday':
         console.log('Enjoy the weekend :D');
         break;
     default:
         console.log('Not a valid day!');
-
     }
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'saturday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
