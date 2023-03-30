11be99485cac24ac88f929201a7695e0ed2efe34
sunghogo <leinad5592@gmail.com>
2023-03-30 11:15:06 -0400

    TCJC2023: Section 2: L26 The switch Statement: Assignment"

--- The Complete Javascript Course 2023/Assignments/assignments.js
+++ The Complete Javascript Course 2023/Assignments/assignments.js
@@ -80,11 +80,34 @@ if (numNeighbours === 1) {
 } else {
     console.log('No borders');
 }
-*/
 
 // L24 Logical Operators
 if ( language === 'english' && population < 50 && !isIsland) {
     console.log(`You should live in ${country} :)`);
 } else {
     console.log(`${country} does not meet your criteria :('`);
+}
+*/
+
+// L26 The switch Statement
+switch(language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    case 'default':
+        console.log('Great language too :D');
+        break;
 }
