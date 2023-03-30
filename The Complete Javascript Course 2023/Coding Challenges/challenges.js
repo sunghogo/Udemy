02c4b1612f940fd9e4ff64155cdd8931146ab33d
sunghogo <leinad5592@gmail.com>
2023-03-29 23:36:54 -0400

    TCJC2023: Section 2: L25 Coding Challenge #3: Individual submission

--- The Complete Javascript Course 2023/Coding Challenges/challenges.js
+++ The Complete Javascript Course 2023/Coding Challenges/challenges.js
@@ -1,3 +1,5 @@
+// Coding Challenge #1 and #2
+/*
 // Test Data 1
 let massMark = 78;
 let heightMark = 1.69;
@@ -31,4 +33,46 @@ if (BMIMark > BMIJohn) {
     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 } else {
     console.log(`John's (${BMIJohn}) is higher than Mark's BMI (${BMIMark})`);
+}
+*/
+
+// Coding Challenge #3
+// Data 1
+let averageDolphins = (96 + 108 + 89) / 3;
+let averageKoalas = (88 + 91 + 110) / 3;
+
+if (averageDolphins > averageKoalas) {
+    console.log("Dolphins win!");
+} else if (averageDolphins === averageKoalas) {
+    console.log("It's a draw!");
+} else {
+    console.log("Koalas win!");
+}
+
+// Bonus Data 1
+averageDolphins = (97 + 112 + 101) / 3;
+averageKoalas = (109 + 95 + 123) / 3;
+
+if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+    console.log("Dolphins win!");
+} else if (averageDolphins === averageKoalas && averageDolphins >= 100) {
+    console.log("It's a draw!");
+} else if (averageDolphins < averageKoalas && averageKoalas >= 100) {
+    console.log("Koalas win!");
+} else {
+    console.log("Nobody wins!");
+}
+
+// Bonus Data 2
+averageDolphins = (97 + 112 + 101) / 3;
+averageKoalas = (109 + 95 + 106) / 3;
+
+if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+    console.log("Dolphins win!");
+} else if (averageDolphins === averageKoalas && averageDolphins >= 100) {
+    console.log("It's a draw!");
+} else if (averageDolphins < averageKoalas && averageKoalas >= 100) {
+    console.log("Koalas win!");
+} else {
+    console.log("Nobody wins!");
 }
