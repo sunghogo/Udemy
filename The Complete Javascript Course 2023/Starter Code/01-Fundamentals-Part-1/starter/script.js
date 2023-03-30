64d834ad60e47991f5df9e05ea7f5f81e01197a2
sunghogo <leinad5592@gmail.com>
2023-03-30 00:00:08 -0400

    TCJC2023: Section 2: L26 The switch Statement: Update

--- The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/01-Fundamentals-Part-1/starter/script.js
@@ -251,7 +251,6 @@ console.log(A && B); // false
 console.log(A || B); // true
 console.log(!A && B); // true
 console.log(A || !B); // false
-*/
 
 // L24 Logical Operators
 const hasDriversLicense = true; // A
@@ -273,3 +272,37 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log('Someone else should drive...');
 }
+*/
+
+// L26 The switch Statement
+// const day = 'monday';
+// const day = 'tuesday';
+// const day = 'wednesday';
+const day = 'thursday';
+
+
+switch(day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break
+    case 'wednesday':
+        break;
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+        break;
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+
+    }
