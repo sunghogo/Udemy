7dc1cd5c0e79f1e1998030ae802023cc9c71cbc2
sunghogo <leinad5592@gmail.com>
2023-03-29 23:15:55 -0400

    TCJC2023: Section 2: L24 Logical Operators: Assignment

--- The Complete Javascript Course 2023/Assignments/assignments.js
+++ The Complete Javascript Course 2023/Assignments/assignments.js
@@ -22,7 +22,7 @@ console.log(population);\
 */
 
 // Lecture 13 let, const and var
-const language = "English";
+const language = "english";
 const country = "United States";
 const continent = "North America";
 let population = "331.9";
@@ -32,24 +32,25 @@ const isIsland = false;
 // Lecture 14: Basic Operators
 const halfPopulation = population / 2;
 population++;
-console.log(population);
+// console.log(population);
 
 const finlandPopulation = 6;
 const populationGreaterFinland = population > finlandPopulation;
-console.log(populationGreaterFinland);
+// console.log(populationGreaterFinland);
 
 const averagePopulation = 33;
 const populationGreaterAverage = population < averagePopulation;
-console.log(populationGreaterAverage);
+// console.log(populationGreaterAverage);
 
 let description = country + " is in " + continent + ", and its " + population + " million people speak " + language; 
-console.log(description);
+// console.log(description);
 
 // L17 Strings and Template Literals
 description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(description);
+// console.log(description);
 
 // L18 Taking Decisions if / else Statements
+/*
 population = 13
 
 if (population > 33) {
@@ -57,15 +58,19 @@ if (population > 33) {
 } else {
     console.log(`${country}'s population is ${33 - population} below average`);
 }
+*/
 
 // L20 Type Conversion and Coercion
+/*
 console.log('9' - '5'); // 4
 console.log('19' - '13' + '17'); // '617'
 console.log('19' - '13' + 17); // 23
 console.log('123' < 57); // false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+*/
 
 // L22 Equality Operators: == vs. ===
+/*
 const numNeighbours = Number(prompt('How many neightbour countries does your country have?'));
 
 if (numNeighbours === 1) {
@@ -74,4 +79,12 @@ if (numNeighbours === 1) {
     console.log('More than 1 border');
 } else {
     console.log('No borders');
+}
+*/
+
+// L24 Logical Operators
+if ( language === 'english' && population < 50 && !isIsland) {
+    console.log(`You should live in ${country} :)`);
+} else {
+    console.log(`${country} does not meet your criteria :('`);
 }
