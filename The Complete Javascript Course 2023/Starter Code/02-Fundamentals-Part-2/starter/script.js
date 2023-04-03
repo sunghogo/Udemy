28cb0b1da6cdc2c920bd47fc5e5ae0d3692d9896
sunghogo <leinad5592@gmail.com>
2023-04-03 11:57:44 -0400

    TCJC2023: Section 3: L36 Functions Calling Other Functions: Starter Code: Function calling other functions sandboxing

--- The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
@@ -64,7 +64,6 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
-*/
 
 
 // L35 Arrow Functions
@@ -82,3 +81,20 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
+*/
+
+
+// L36 Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
