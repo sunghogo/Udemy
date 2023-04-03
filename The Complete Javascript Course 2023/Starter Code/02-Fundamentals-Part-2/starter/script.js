1939873c447db3d41ffb739196d480a3360680cd
sunghogo <leinad5592@gmail.com>
2023-04-03 11:39:44 -0400

    TCJC2023: Section 3: L35 Arrow Functions: Starter Code: Arrow function sandboxing

--- The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
@@ -40,7 +40,6 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
-*/
 
 
 // L34 Function Declarations vs. Expressions
@@ -60,9 +59,26 @@ function calcAge1(birthYear) {
 
 const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
-
 }
 
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+*/
+
+
+// L35 Arrow Functions
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
