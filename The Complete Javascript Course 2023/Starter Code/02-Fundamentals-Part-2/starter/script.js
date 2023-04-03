6e01d905c7e98d52db0337907806b36f0341e10d
sunghogo <leinad5592@gmail.com>
2023-04-03 10:27:00 -0400

    TCJC2023: Section 3: L33 Functions: Starter Code: Functions sandboxing

--- The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
@@ -18,3 +18,26 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 
 // L33 Functions
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger(23);
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
