0b619e3e0e2f4c77687131b2c5eee8f58a5af5e6
sunghogo <leinad5592@gmail.com>
2023-04-03 16:45:07 -0400

    TCJC2023: Section 3: L41 Coding Challenge #2: Coding Challenge: Lecture example amendments

--- The Complete Javascript Course 2023/Coding Challenges/challenges2.js
+++ The Complete Javascript Course 2023/Coding Challenges/challenges2.js
@@ -45,16 +45,14 @@ createWinner(avgDolphins, avgKoalas);
 
 // Coding Challenge #2
 const calcTip = bill => (50 <= bill && bill <= 300) ? bill * 0.15 : bill * 0.2;
-console.log(calcTip(100));
+// console.log(calcTip(100));
 
 const bills = [125, 555, 44];
-
 const tips = [
     calcTip(bills[0]), 
     calcTip(bills[1]), 
     calcTip(bills[2])
 ]
-
 const total = [
     bills[0] + tips[0],
     bills[1] + tips[1],
