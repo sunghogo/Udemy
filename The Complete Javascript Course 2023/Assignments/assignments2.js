37a36b6c56876c93643cf35c28cc57ee55d8ca14
sunghogo <leinad5592@gmail.com>
2023-04-03 10:38:25 -0400

    TCJC2023: Section 3: L33 Functions: Assignment

--- The Complete Javascript Course 2023/Assignments/assignments2.js
+++ The Complete Javascript Course 2023/Assignments/assignments2.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+
+const descriptionUS = describeCountry('United States', 331, 'Washington, D.C.');
+const descriptionFinland = describeCountry('Finalnd', 6, 'Helsinki');
+const descriptionPortugal = describeCountry('Portugal', 10, 'Lisbon');
+
+console.log(descriptionUS, descriptionFinland, descriptionPortugal);
