e8cee525605911c804848184d94b8aff9a5c28e1
sunghogo <leinad5592@gmail.com>
2023-04-03 10:03:08 -0400

    TCJC2023: Section 3: L32 Activating Strict Mode: Starter Code: Strict mode sandboxing

---
+++ The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+// L32 Activating Strict Mode
+// 'use strict';
+
+// Check for undefined variables
+let hasDriversLicense = false;
+const passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// Check for reserved variable names
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
