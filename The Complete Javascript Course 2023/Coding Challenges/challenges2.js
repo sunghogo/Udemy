a230addbc150451612f8cb82d9d17eafbf696ba1
sunghogo <leinad5592@gmail.com>
2023-04-03 16:40:59 -0400

    TCJC2023: Section 3: L41 Coding Challenge #2: Coding Challenge: Individual Attemppt

--- The Complete Javascript Course 2023/Coding Challenges/challenges2.js
+++ The Complete Javascript Course 2023/Coding Challenges/challenges2.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // Coding Challenge #1
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
@@ -40,3 +41,24 @@ avgDolphins = calcAverage(scoreDolphins1, scoreDolphins2, scoreDolphins3);
 avgKoalas = calcAverage(scoreKoalas1, scoreKoalas2, scoreKoalas3);
 
 createWinner(avgDolphins, avgKoalas);
+*/
+
+// Coding Challenge #2
+const calcTip = bill => (50 <= bill && bill <= 300) ? bill * 0.15 : bill * 0.2;
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+
+const tips = [
+    calcTip(bills[0]), 
+    calcTip(bills[1]), 
+    calcTip(bills[2])
+]
+
+const total = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+]
+
+console.log(bills, tips, total);
