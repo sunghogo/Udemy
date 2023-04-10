a5d3c3b4c95a865825471da8e930cf5a5427f737
sunghogo <leinad5592@gmail.com>
2023-04-10 13:35:08 -0400

    TCJC2023: Section 3: L48 Looping Backwards and Loops in Loops: Starter Code: Backwards iteration and nested loop sandboxing

--- The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
@@ -332,7 +332,6 @@ console.log(jonas.getSummary2())
 for (let rep = 1; rep <= 30; rep++) {
     console.log(`Lifting weights repetition ${rep} 🏋️`);
 }
-*/
 
 
 // L47 Looping Arrays, Breaking and Continuing
@@ -386,4 +385,28 @@ for (let i = 0; i < jonas.length; i++) {
 
     console.log(jonas[i], typeof jonas[i]);
 }
+*/
 
+
+// L48 Looping Backwards and Loops in Loops
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+];
+
+// 0, 1, ..., 4
+// 4, 3, ...., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`-------- Start exercise ${exercise}`);
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️`);
+    }
+}
