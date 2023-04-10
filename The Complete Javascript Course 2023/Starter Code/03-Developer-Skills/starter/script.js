cbc0c03aea354e8c2de035e6f090f69680c75667
sunghogo <leinad5592@gmail.com>
2023-04-10 17:44:48 -0400

    TCJC2023: Section 5: L56 Installing Node.js and Setting Up a Dev Environment: Starter Code: Live Server sandboxing

--- The Complete Javascript Course 2023/Starter Code/03-Developer-Skills/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/03-Developer-Skills/starter/script.js
@@ -1,8 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const xxasdasdz = 23;
+// L55 Setting up Prettier and VS Code
+const x = 23;
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
-console.log(xxasdasdz);
+console.log(x);
+
+// BUG, FIXME
+
+// L56 Installing Node.js and Setting Up a Dev Environment
+console.log(calcAge(1992));
