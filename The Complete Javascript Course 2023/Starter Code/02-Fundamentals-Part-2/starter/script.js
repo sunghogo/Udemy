d07458e462fc70afd28b7126d840a675ae4f5407
sunghogo <leinad5592@gmail.com>
2023-04-10 13:15:25 -0400

    TCJC2023: Section 3: L47: Looping Arrays, Breaking and Continuing: Starter Code: array looping, continue/break sandboxing

--- The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/02-Fundamentals-Part-2/starter/script.js
@@ -306,7 +306,6 @@ console.log(jonas.age);
 
 console.log(jonas.getSummary())
 console.log(jonas.getSummary2())
-*/
 
 
 // L46 Iteration: The for Loop
@@ -333,3 +332,58 @@ console.log(jonas.getSummary2())
 for (let rep = 1; rep <= 30; rep++) {
     console.log(`Lifting weights repetition ${rep} 🏋️`);
 }
+*/
+
+
+// L47 Looping Arrays, Breaking and Continuing
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];    
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+
+for(let i = 0; i < jonas.length; i++) {
+    // Reading for jonas array
+    console.log(jonas[i], typeof jonas[i]);
+
+    // Filling types array
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+console.log('---ONLY STRINGS---');  
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('---BREAK WITH NUMBER---');  
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
