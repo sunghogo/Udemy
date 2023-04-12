8bf512ad9dfaf6fd81586a0d512ddd1c0b8342a1
sunghogo <leinad5592@gmail.com>
2023-04-12 17:26:00 -0400

    TCJC2023: Section 7: L80 Working With Classes: Starter Code: class and .classList method sandboxing

--- The Complete Javascript Course 2023/Starter Code/06-Modal/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/06-Modal/starter/script.js
@@ -1,13 +1,29 @@
 'use strict';
 
 // L79 PROJECT #2: Modal Window
-const model = document.querySelector('.modal');
+// L80 Working With Classes
+
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  // modal.style.display = 'block';
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
