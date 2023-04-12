0e95f351d2f7110270002e20f2e55434f39d07d1
sunghogo <leinad5592@gmail.com>
2023-04-12 12:40:12 -0400

    TCJC2023: Section 7: L72 Selecting and Manipulating Elements: Starter Code: Using document.querySelector(), .textContent, and .value

--- The Complete Javascript Course 2023/Starter Code/05-Guess-My-Number/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/05-Guess-My-Number/starter/script.js
@@ -3,3 +3,12 @@
 // L70 PROJECT #1: Guess My Number!
 
 console.log(document.querySelector('.message').textContent);
+
+// L72 Selecting and Manipulating Elements
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
