229244de2e1d35aede65bd738c68b2159bf88c3a
sunghogo <leinad5592@gmail.com>
2023-04-12 13:05:03 -0400

    TCJC2023: Section 7: L73 Handling Click Events: Starter Code: .addEventListener() sandboxing

--- The Complete Javascript Course 2023/Starter Code/05-Guess-My-Number/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/05-Guess-My-Number/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
+/*
 // L70 PROJECT #1: Guess My Number!
-
 console.log(document.querySelector('.message').textContent);
 
 // L72 Selecting and Manipulating Elements
@@ -12,3 +12,25 @@ document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+// L73 Handling Click Events
+const x = function () {
+  console.log(23);
+};
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+
+//   document.querySelector('.message').textContent = '🎉 Correct Number!';
+// });
+
+document.querySelector('.check').addEventListener('click', function () {
+  //   const guess = document.querySelector('.guess').value;
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+});
