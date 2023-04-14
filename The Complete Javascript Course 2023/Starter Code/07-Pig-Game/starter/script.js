2410fe1fb6b3cbfcbc628debc1fd3dba14fb7100
sunghogo <leinad5592@gmail.com>
2023-04-14 15:19:50 -0400

    TCJC2023: Section 7: L86. Resetting the Game: Starter Code: Implemented init() to initialize/reset game

--- The Complete Javascript Course 2023/Starter Code/07-Pig-Game/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/07-Pig-Game/starter/script.js
@@ -18,15 +18,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Strating conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
+// Starting conditions
 const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let currentScore, activePlayer, playing;
+
+const init = function () {
+  scores[0] = scores[1] = 0;
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner', 'player--active');
+  player0El.classList.add('player--active');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -35,6 +46,9 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+// Initialize
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -87,19 +101,5 @@ btnHold.addEventListener('click', function () {
 
 // New game functionality
 btnNew.addEventListener('click', function () {
-  // Reset State
-  playing = true;
-  activePlayer = 0;
-  player0El.classList.remove('player--winner', 'player--active');
-  player1El.classList.remove('player--winner', 'player--active');
-  player0El.classList.add('player--active');
-  diceEl.classList.remove('hidden');
-
-  // Reset Score
-  scores[0] = scores[1] = 0;
-  currentScore = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+  init();
 });
