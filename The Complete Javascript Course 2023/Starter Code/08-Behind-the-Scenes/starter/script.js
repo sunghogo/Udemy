f7253723ad68c42e783b04d1572e99493fd5509e
sunghogo <leinad5592@gmail.com>
2023-04-16 15:41:41 -0400

    TCJC2023: Section 8: L95 Hoisting and TDZ in Practice: Starter Code: Hoisting and TDZ sandboxing

--- The Complete Javascript Course 2023/Starter Code/08-Behind-the-Scenes/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/08-Behind-the-Scenes/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
+/*
 // L93 Scoping in Practice
-
 // Parent function scope
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
@@ -39,6 +39,62 @@ function calcAge(birthYear) {
 }
 
 const firstName = 'Jonas'; // Global Variable
-calcAge(1991); // Code in function only executed once it is called
+calcAge((1991); // Code in function only executed once it is called
 // console.log(age); // Error: Cannot access to a child scope
 // printAge(); // Error: In global scope, do not have access to any variables defined in child scopes
+*/
+
+// L95 Hoisting and TDZ in Practice
+
+// Variables Hoisting
+console.log(me); // Undefined
+// console.log(job); // ReferenceError TDZ
+// console.log(year); // ReferenceError TDZ
+
+var me = 'Jonas';
+let job = 'teacher ';
+const year = 1991;
+
+// Functions Hoisting
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+// ReferenceError TDZ
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+// // ReferenceError TDZ
+// const addArrow = (a, b) => a + b;
+
+// // TypeError same as calling undefined()
+// var addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// ReferenceError same as calling undefined()
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
