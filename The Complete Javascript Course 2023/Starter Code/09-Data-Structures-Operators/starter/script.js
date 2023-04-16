47c8699e843402d401f2a30091e77490b79dbff6
sunghogo <leinad5592@gmail.com>
2023-04-16 19:13:56 -0400

    TCJC2023: Section 9: L103 Destructuring Arrays: Starter Code: Destructuring array sandboxing

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,49 @@ const restaurant = {
     },
   },
 };
+
+// L103 Destructuring Arrays:
+// Without Destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// With Destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+// const [first, , third] = restaurant.categories;
+// console.log(first, third);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching Variables Without Destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// Switching Variables With Destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+// console.log(restaurant.order(2, 0));
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
