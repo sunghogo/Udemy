f43e54054a3541b43ec17c47c0fdbcb324ec0196
sunghogo <leinad5592@gmail.com>
2023-04-21 18:29:44 -0400

    TCJC2023: Section 9: 109 Logical Assignment Operators: Typo fix and update

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -304,8 +304,8 @@ rest2.numGuests ??= 10;
 // rest2.owner = rest2.owner && '<ANONYMOUS>';
 
 // AND assignment operator
-rest1.owner &&= '<ANONYMOUS'>
-rest2.owner &&= '<ANONYMOUS'>
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
 
 console.log(rest1);
 console.log(rest2);
