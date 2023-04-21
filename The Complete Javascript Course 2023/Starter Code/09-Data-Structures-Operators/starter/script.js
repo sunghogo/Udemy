b38621dfbb933dc9c006ba4aa856ad2fe3873100
sunghogo <leinad5592@gmail.com>
2023-04-21 16:31:54 -0400

    TCJC2023: Section 9: L105 The Spread Operator (...): Starter Code: Spread operator sandboxing

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,21 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
@@ -27,19 +42,10 @@ const restaurant = {
     );
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
   },
 };
 
@@ -145,3 +151,43 @@ console.log(badNewArr);
 
 const newArr = [1, 2, ...arr];
 console.log(newArr);
+console.log(...newArr); // 1, 2, 7, 8, 9
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets, NOT objects
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+console.log(...str); // 'j', 'o', 'n', 'a', 's'
+console.log('j', 'o', 'n', 'a', 's');
+// console.log(`${...str} Schmedtmann`); // Doesn't work
+
+// Real World Example
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]); // Old way of extracting values from arrays
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { founding: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name); // Name is different, so we made a copy
+console.log(restaurant.name);
