65567717ebcd3f1d57b2bb2553a992447a1dd8e3
sunghogo <leinad5592@gmail.com>
2023-04-23 15:56:44 -0400

    TCJC2023: Section 9: L111 Looping Arrays: The for-of Loop: Starter Code: for-of loop sandboxing

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -313,3 +313,14 @@ console.log(rest2);
 // L111 Looping Arrays: The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu) console.log(item);
+
+// Old method
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+
+// Destructuring method
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+// console.log([...menu.entries()]);
