4f08f449aa70b54089c14d1e8cc668040db59acb
sunghogo <leinad5592@gmail.com>
2023-04-24 15:18:41 -0400

    TCJC2023: Section 9: L122 Working With Strings - Part 2: Starter Code: Additional string methods sandboxing

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -507,7 +507,6 @@ const map = new Map([
 
 const func = map.get('func');
 func();
-*/
 
 // L121 Working With Strings - Part 1
 const airline = 'TAP Air Portugal';
@@ -550,3 +549,63 @@ console.log(new String('Jonas')); // Similar to our string box
 console.log(typeof new String('Jonas')); // Object
 
 console.log(typeof new String('Jonas').slice(1)); // String
+*/
+
+// L122 Working With Strings - Part 2
+const airlines = 'TAP Air Portugal';
+
+console.log(airlines.toLowerCase());
+console.log(airlines.toUpperCase());
+
+// Fix capitailzation in name
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase + passengerLower.slice[1];
+console.log(passengerCorrect);
+
+// Comparing email
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23, Boarding door 23!I';
+console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replaceAll(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family!');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a Laptop, some Food and a Pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
