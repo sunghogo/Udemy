d94b1c184428f3b064e1f863b267337fa73af387
sunghogo <leinad5592@gmail.com>
2023-04-24 10:54:55 -0400

    TCJC2023: Section 9: L117 Maps: Fundamentals: Starter Code: Map methods and usages sandboxing

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -419,3 +419,38 @@ console.log(new Set('jonasschmedtmann').size);
 */
 
 // L117 Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', [...restaurant.categories])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed ):');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get('1'));
+console.log(rest.get(1));
+
+const time = 24;
+console.log(`${rest.get(rest.get('open') < time && time < rest.get('close'))}`);
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+console.log(rest);
+console.log(rest.size);
+
+// This does not work because objects are different
+// rest.set([1, 2], 'Test');
+// console.log(rest.get([1, 2]));
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+
+rest.set(document.querySelector('h1'), 'Heading');
