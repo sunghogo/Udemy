b741391e6a2bde88d024ebc8053c3726dcda8c8c
sunghogo <leinad5592@gmail.com>
2023-04-24 17:55:12 -0400

    TCJC2023: Section 9: L123 Working With Strings - Part 3: Starter Code: Additional atring methods sandboxing

--- The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/09-Data-Structures-Operators/starter/script.js
@@ -549,7 +549,6 @@ console.log(new String('Jonas')); // Similar to our string box
 console.log(typeof new String('Jonas')); // Object
 
 console.log(typeof new String('Jonas').slice(1)); // String
-*/
 
 // L122 Working With Strings - Part 2
 const airlines = 'TAP Air Portugal';
@@ -609,3 +608,57 @@ const checkBaggage = function (items) {
 checkBaggage('I have a Laptop, some Food and a Pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
+*/
+
+// L123 Working With Strings - Part 3
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // Method 1
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    // Method 2
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(25, '+').padEnd(30, '+'));
+
+// Real-life example
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad weather... All depaartures delayed...';
+console.log(typeof message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
