3129497f605819966f3352cc53a9bfba34605571
sunghogo <leinad5592@gmail.com>
2023-04-27 11:38:38 -0400

    TCJC2023: Section 10: L131 Functions Accepting Callback Functions: Update

--- The Complete Javascript Course 2023/Starter Code/10-Functions/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/10-Functions/starter/script.js
@@ -66,3 +66,23 @@ checkIn(flight, jonas); // Wrong passport!
 */
 
 // L130 First-Class and Higher-Order Functions
+
+// L131 Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
