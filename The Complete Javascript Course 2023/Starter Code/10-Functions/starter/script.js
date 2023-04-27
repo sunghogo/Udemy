aefb455ac8229ed255dfe0105b6109c0afa53a6a
sunghogo <leinad5592@gmail.com>
2023-04-26 23:15:47 -0400

    TCJC2023: Section 9: L129 How Passing Arguments Works: Value vs. Reference: Starter Code: Function arguments references vs values sandboxing

--- The Complete Javascript Course 2023/Starter Code/10-Functions/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // L128 Default Parameters
 const bookings = [];
 
@@ -29,3 +30,37 @@ createBooking('LH123', 5);
 
 // createBooking('LH123', 1000);
 createBooking('LH123', undefined, 1000);
+*/
+
+// L129 How Passing Arguments Works: Value vs. Reference
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas); // Check In
+console.log(flight); // LH234 unchanged
+console.log(jonas); // 'Mr. Jonas Schmedtmann' changed
+
+// Is the same sa doing...
+const flightNum = flight; // passing primitive type
+const passenger = jonas; // passing object
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * e + 11);
+};
+
+newPassport(jonas); // original object is changed
+checkIn(flight, jonas); // Wrong passport!
