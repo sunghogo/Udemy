636692010f0b994b6f97ba7116d3af1b763a30b6
sunghogo <leinad5592@gmail.com>
2023-04-28 17:52:24 -0400

    TCJC2023: Section 10: L135 Coding Challenge #1: Coding Challenge: Leture Example Addition

--- The Complete Javascript Course 2023/Coding Challenges/challenge5.js
+++ The Complete Javascript Course 2023/Coding Challenges/challenge5.js
@@ -49,26 +49,36 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
-  // 3.
-  displayResults(type = 'array') {
-    type === 'array'
-      ? console.log(this.answers)
-      : console.log(`Poll results are ${this.answers}`);
-  },
   // 1. 4.
   registerNewAnswer() {
+    // Get answer
     const answer = Number(
-      prompt(`What is your favourite programming language?
-    0: JavaScript
-    1: Python
-    2: Rust
-    3: C++
-    (Write option number)`)
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
     );
-    0 <= answer && answer < 4
-      ? this.answers[answer]++
-      : console.log('Invalid answer!');
+    console.log(answer);
+
+    // Register answer
+    // Lecture Example
+    // typeof answer === 'number' &&
+    //   answer < this.answers.length &&
+    //   this.answers[answer]++;
+
+    // Indivdual Example
+    (0 <= answer && answer < this.answers.length && ++this.answers[answer]) ||
+      console.log('Invalid answer!');
+
     this.displayResults();
+    this.displayResults('string');
+  },
+  // 3.
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
 
@@ -83,7 +93,7 @@ const testData1 = {
 const testData2 = {
   answers: [1, 5, 3, 9, 6, 1],
 };
-poll.displayResults.call(testData1);
-poll.displayResults.call(testData1, 'string');
-poll.displayResults.call(testData2);
-poll.displayResults.call(testData2, 'string');
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
