8cc419e8e368072bafc7dde7128824829e1467f7
sunghogo <leinad5592@gmail.com>
2023-05-01 12:03:52 -0400

    TCJC2023: Section 11: L143 The new at Method: Starter Code: at() sandboxing

--- The Complete Javascript Course 2023/Starter Code/11-Arrays-Bankist/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/11-Arrays-Bankist/starter/script.js
@@ -74,6 +74,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
 // L142 Simple Array Methods
 function reset() {
   return ['a', 'b', 'c', 'd', 'e'];
@@ -113,3 +114,17 @@ console.log([...arr, ...arr2]);
 console.log(typeof letters.join());
 console.log(letters.join());
 console.log(letters.join(' - '));
+*/
+
+// 143s The new at Method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
