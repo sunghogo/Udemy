1ebd79fa2cf2e390a809fd4d66baf18f462c2b13
sunghogo <leinad5592@gmail.com>
2023-05-08 16:45:50 -0400

    TCJC2023: Section 11: L161 some and every: Starter Code: Loan functionality, .some() and .every() array methods sandboxing

--- The Complete Javascript Course 2023/Starter Code/11-Arrays-Bankist/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/11-Arrays-Bankist/starter/script.js
@@ -207,6 +207,25 @@ btnClose.addEventListener('click', function (e) {
   // Clear fields
   inputCloseUsername.value = inputClosePin.value = '';
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+
+    // Clear input fields
+    inputLoanAmount.value = '';
+    inputLoanAmount.blur();
+  }
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -464,3 +483,25 @@ console.log('jonas'.at(-1));
   console.log(accountFilter);
 }
 */
+
+// L161 some and every
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 1500);
+console.log(anyDeposits);
+
+// EVERY: CONDITION
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Seperate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
