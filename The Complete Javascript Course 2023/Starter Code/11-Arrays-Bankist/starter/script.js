1e77414137601e1bd4eb1a45d6eba712e54caaab
sunghogo <leinad5592@gmail.com>
2023-05-11 10:55:56 -0400

    TCJC2023: Section 11: L164 More Ways of Creating and Filling Arrays: Starter Code: .fill() and Array.from() methods sandboxing

--- The Complete Javascript Course 2023/Starter Code/11-Arrays-Bankist/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/11-Arrays-Bankist/starter/script.js
@@ -561,3 +561,44 @@ console.log(movements);
 movements.sort((a, b) => b - a);
 console.log(movements);
 */
+
+// L164 More Ways of Creating and Filling Arrays
+// Old way to create and fill arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+console.log(typeof x[0]);
+console.log(x.map(() => 5)); // Does NOT fill
+
+// .fill()
+x.fill(1, 3, 5);
+console.log(x);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  // You can also construct the array using the spread operator
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')].map(
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI2);
+});
