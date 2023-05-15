6384391a27c42d52d22f2fc6cba0a06b6ab67658
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-15 19:45:38 -0400

    TCJC2023: Section 12: L170 Converting and Checking Numbers: Starter Code: Number type and Number namespace functions sandboxing

--- The Complete Javascript Course 2023/Starter Code/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,38 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// L170 Converting and Checking Numbers
+console.log(23 === 23.0);
+
+// Base 10: 0 to 9. 1/10 = 0.1. 3/10 = 3.33333
+// Binary base 2: 0 and 1.
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); // false, even though it's true
+
+// Conversion
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('30px')); // 30
+console.log(Number.parseInt('e23')); // NaN
+
+console.log(Number.parseFloat('2.5rem')); // 2.5
+console.log(Number.parseInt('2.5rem')); // 2
+
+// console.log(parseFloat('2.5rem'));
+
+console.log(Number.isNaN(20)); // 20 -> false
+console.log(Number.isNaN('20')); // 'string' -> false
+console.log(Number.isNaN(+'20X')); // 'NaN' -> true
+console.log(Number.isNaN(23 / 0)); // 'Infinity' -> false
+
+console.log(Number.isFinite(20)); // 20 -> true
+console.log(Number.isFinite('20')); // 'string' -> false
+console.log(Number.isFinite(+'20X')); // 'NaN' ->false
+console.log(Number.isFinite(23 / 0)); // 'Infinity' -> false
+
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(23 / 0)); // false
