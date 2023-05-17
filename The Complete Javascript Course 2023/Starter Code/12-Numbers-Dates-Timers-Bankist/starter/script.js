9175a3e60d39ffb3244469fd15708ae7f78c74fe
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-17 19:27:04 -0400

    TCJC2023: Section 12: L179 Internationalizing Numbers (Intl): Starter Code: Currencies and movements are now internationalized according to account locale and currency

--- The Complete Javascript Course 2023/Starter Code/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -83,6 +83,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // L176 Adding Dates to "Bankist" App
 // L177 Operations With Dates
 // L178 Internationalizing Dates (Intl)
+// L179 Internationalizing Numbers (Intl)
 
 // Functions
 const formatMovementDate = function (date, locale) {
@@ -103,6 +104,13 @@ const formatMovementDate = function (date, locale) {
   return new Intl.DateTimeFormat(locale).format(date);
 };
 
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -116,13 +124,15 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
 
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
          <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -132,19 +142,19 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -154,7 +164,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -533,4 +543,22 @@ console.log(future);
   );
   console.log(days1);
 }
+
+// L179 Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+const options = {
+  style: 'currency',
+  unit: 'mile-per-hour',
+  currency: 'USD',
+  // useGrouping: false
+};
+
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria   :', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(
+  'Browser :',
+  new Intl.NumberFormat(navigator.language, options).format(num)
+);
 */
