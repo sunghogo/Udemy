e98d970da1d2c303535bb6b218a32b35a75f6eaa
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-17 19:57:42 -0400

    TCJC2023: Section 12: L180 Timers: setTimeout and setInterval: Starter Code: Loans now take 2.5s to approve, timers sandboxing with setTimeout(), setInterval(), clearTimeout()

--- The Complete Javascript Course 2023/Starter Code/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -281,14 +281,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -562,3 +564,26 @@ console.log(
   new Intl.NumberFormat(navigator.language, options).format(num)
 );
 */
+
+// L180 Timers: setTimeout and setInterval
+// setTimeout()
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval()
+setInterval(function () {
+  const now = new Date();
+  const time = new Intl.DateTimeFormat(navigator.language, {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  }).format(now);
+  console.log(time);
+}, 1000);
