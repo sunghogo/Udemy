8c931c115b6d52cb73b36775931ca14aa4fb359d
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-18 17:12:55 -0400

    TCJC2023: Section 16: L254 Handling Rejected Promises: Starter Code: getCoutnryData() now handles errors using renderError() and .catch(), default behavior set using .finally()

--- The Complete Javascript Course 2023/Starter Code/16-Asynchronous/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/16-Asynchronous/starter/script.js
@@ -3,12 +3,37 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${+(
+              data.population / 1_000_000
+            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+         </div>
+      </article>
+      `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  //   countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //   countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
 /*
 // L248 Our First AJAX Call: XMLHttpRequest
 // L250 Welcome to Callback Hell
 // L251 Promises and the Fetch API
-
+{
 const renderCountry = function (data, className = '') {
   const html = `
     <article class="country ${className}">
@@ -76,33 +101,14 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+}
 */
 
 // L251 Promises and the Fetch API
 // L252 Consuming Promises
 // L253 Chaining Promises
+// L254 Handling Rejected Promises
 {
-  const renderCountry = function (data, className = '') {
-    const html = `
-      <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${+(
-              data.population / 1_000_000
-            ).toFixed(1)} million people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-         </div>
-      </article>
-      `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  };
-
   //   const getCountryData = function () {
   //     const request = fetch('https://restcountries.com/v2/name/portugal')
   //       .then(function (response) {
@@ -129,8 +135,19 @@ setTimeout(() => {
         return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
       })
       .then(response => response.json())
-      .then(data => renderCountry(data, 'neighbour'));
+      .then(data => renderCountry(data, 'neighbour'))
+      .catch(err => {
+        console.error(`${err} 💥💥💥`);
+        renderError(`Something went wrong 💥💥💥 ${err.message}. Try Again!`);
+      })
+      .finally(() => {
+        countriesContainer.style.opacity = 1;
+      });
   };
 
-  getCountryData('portugal');
+  btn.addEventListener('click', function () {
+    getCountryData('portugal');
+  });
+
+  getCountryData('unknowncountry');
 }
