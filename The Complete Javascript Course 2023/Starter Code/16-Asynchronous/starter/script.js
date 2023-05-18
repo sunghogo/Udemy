4bc4fd8a51d695b7edb809426b7508f1749f81c6
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-18 16:26:41 -0400

    TCJC2023: Section 16: L252 Consuming Promises: Notes: Reimplemented getCountryData() using fetch() and .then() to consume promises

--- The Complete Javascript Course 2023/Starter Code/16-Asynchronous/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/16-Asynchronous/starter/script.js
@@ -79,6 +79,46 @@ setTimeout(() => {
 */
 
 // L251 Promises and the Fetch API
+// L252 Consuming Promises
+{
+  const renderCountry = function (data, className = '') {
+    const html = `
+      <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${+(
+              data.population / 1_000_000
+            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+         </div>
+      </article>
+      `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  };
 
-const request = fetch('https://restcountries.com/v2/name/portugal');
-console.log(request);
+  //   const getCountryData = function () {
+  //     const request = fetch('https://restcountries.com/v2/name/portugal')
+  //       .then(function (response) {
+  //         console.log(response);
+  //         return response.json();
+  //       })
+  //       .then(function (data) {
+  //         console.log(data);
+  //         renderCountry(data[0]);
+  //       });
+  //   };
+
+  const getCountryData = function () {
+    const request = fetch('https://restcountries.com/v2/name/portugal')
+      .then(response => response.json())
+      .then(data => renderCountry(data[0]));
+  };
+
+  getCountryData('portugal');
+}
