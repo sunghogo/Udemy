c26425144132a541c9113afc439f30b7910b7904
sunghogo <leinad5592@gmail.com>
2023-05-24 12:34:50 -0400

    TCJC2023: Section 17: L274 The Module Pattern: Starter Code: Module pattern sandboxing

--- The Complete Javascript Course 2023/Starter Code/17-Modern-JS-Modules-Tooling/starter/script.js
+++ The Complete Javascript Course 2023/Starter Code/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,6 @@
+// Modules are by default in 'use strict'
+
+/*
 // L272 Exporting and Importing in ES6 Modules
 // Importing module
 
@@ -47,3 +50,36 @@ lastPost.then(last => console.log(last));
 // Not very clean
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+// 274. The Module Pattern
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); // Not accessible because not exported and made private
+console.dir(ShoppingCart2);
