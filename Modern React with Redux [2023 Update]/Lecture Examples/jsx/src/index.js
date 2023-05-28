77b3fcdaadce7860a99f4569f0b34339b1d2a6fa
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-28 11:02:36 -0400

    MRR2023: Section 1: L18 Converting HTML to JSX: Prop names are camelCase

--- Modern React with Redux [2023 Update]/Lecture Examples/jsx/src/index.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/jsx/src/index.js
@@ -10,28 +10,28 @@ const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
+  /* 
   // JSX can reference variables and other expressions using {}, but can only properly render numbers, strings, arrays?
-
-  // let message = "Bye there!";
-  // if (Math.random() > 0.5) {
-  //   message = "Hello there!";
-  // }
-
-  // const date = new Date();
-  // const time = date.toLocaleTimeString();
+  let message = "Bye there!";
+  if (Math.random() > 0.5) {
+    message = "Hello there!";
+  }
+  const date = new Date();
+  const time = date.toLocaleTimeString();
   // return <h1>{time}</h1>;
-  // return <h1>{new Date().toLocaleTimeString()}</h1>;
-
-  // // You can mix in plain text and multiple variables similar to template literals in JS
-  // const name = "Samantha";
-  // const age = 23;
-
-  // return (
-  //   <h1>
-  //     Hi, my name is {name} and my age is {age}
-  //   </h1>
-  // );
-
+  return <h1>{new Date().toLocaleTimeString()}</h1>; 
+  */
+  /*  
+  // You can mix in plain text and multiple variables similar to template literals in JS
+  const name = "Samantha";
+  const age = 23;
+  return (
+    <h1>
+      Hi, my name is {name} and my age is {age}
+    </h1>
+  );
+ */
+  /* 
   // JSX Prop(ertie)s System is similar to HTML attributes, and customize an element using property=value pairs
   // Trying to display an object doesn't work, but providing it to a prop as options is OK
   const inputType = "number";
@@ -49,6 +49,10 @@ function App() {
       alt={message}
     />
   );
+ */
+  // Names/vaues of attributes for JSX can be/are slightly different in JSX:
+  // 1. Prop names are camelCase
+  return <textarea autoFocus={true} />;
 }
 
 // 5) Show the component on the screen
