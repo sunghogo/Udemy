6b359c7a1cb0ecc1a70d3d07871cd3bf3844b757
sunghogo <leinad5592@gmail.com>
2023-06-04 15:03:42 -0400

    MRR2023: Section 3: L34 Including Images: Importing and srcing images

--- Modern React with Redux [2023 Update]/Lecture Examples/pdas/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/pdas/src/App.js
@@ -1,4 +1,12 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+// We can import images in our project like regular components
+// Images less than 9.7kb is converted into base64 and is served up as inline string, otherwise images larger than 9.7kb gets served up as separate files on the development server
+// console.log(AlexaImage);
+// console.log(SiriImage);
 
 const Profiles = {
   Alexa: { title: "Alexa", handle: "@alexa99" },
--- Modern React with Redux [2023 Update]/Lecture Examples/pdas/src/ProfileCard.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/pdas/src/ProfileCard.js
@@ -1,3 +1,4 @@
+// We can destructure the props object directly in the arguments to clean up code
 function ProfileCard({ title, handle }) {
   //   const { title, handle } = props;
 
