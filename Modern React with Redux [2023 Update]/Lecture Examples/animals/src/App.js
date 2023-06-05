2d3e7b7c09e40e24d0982544f6fec202ff28c956
sunghogo <leinad5592@gmail.com>
2023-06-05 12:33:05 -0400

    MRR2023: Section 4: L51 Picking a Random Element: Created getRandomAnimal function to randomly generate animal, and refactored click handler

--- Modern React with Redux [2023 Update]/Lecture Examples/animals/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/animals/src/App.js
@@ -1,28 +1,21 @@
 import { useState } from "react";
 
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 function App() {
   const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    const prob = Math.random();
-    let type;
-    while (!type && type !== "retry") {
-      type =
-        prob < 0.33
-          ? "cow"
-          : prob < 0.66
-          ? "bird"
-          : prob < 0.99
-          ? "horse"
-          : "retry";
-    }
-    setAnimals([...animals, type]);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>{animals}</div>
+      <div>{animals.join(" ")}</div>
     </div>
   );
 }
