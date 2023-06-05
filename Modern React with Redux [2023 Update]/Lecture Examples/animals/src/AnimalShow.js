d1886ccb3fd6976f9ac594b46c930fa8c44c59ec
sunghogo <leinad5592@gmail.com>
2023-06-05 13:23:04 -0400

    MRR2023: Section 4: L57 Finalizing Styling: Finished styling App and AnimalShow components

--- Modern React with Redux [2023 Update]/Lecture Examples/animals/src/AnimalShow.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/animals/src/AnimalShow.js
@@ -26,9 +26,14 @@ function AnimalShow({ type }) {
   };
 
   return (
-    <div onClick={handleClick}>
-      <img src={svgMap[type]} alt={type} />
-      <img src={heart} alt="heart" style={{ width: 10 + 10 * clicks + "px" }} />
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" src={svgMap[type]} alt={type} />
+      <img
+        className="heart"
+        src={heart}
+        alt="heart"
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
     </div>
   );
 }
