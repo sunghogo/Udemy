dffb7fe2578a863232ecb41380bda517982b5a09
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-05 09:43:23 -0400

    MRR2023: Section 4: L44 Variations on Event Handlers: Refactored mouse click callback directly into event handler

--- Modern React with Redux [2023 Update]/Lecture Examples/animals/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/animals/src/App.js
@@ -1,11 +1,9 @@
 function App() {
-  const handleClick = () => {
-    console.log("Button was clicked!");
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>Add Animal</button>
+      <button onClick={() => console.log("Button was clicked!")}>
+        Add Animal
+      </button>
     </div>
   );
 }
