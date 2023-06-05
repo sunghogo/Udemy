37a71b437b7df1409c0db53c3509dbb224b161fa
sunghogo <leinad5592@gmail.com>
2023-06-05 11:46:43 -0400

    MRR2023: Section 4: L47 More on State: Reintroduced click handler function

--- Modern React with Redux [2023 Update]/Lecture Examples/animals/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/animals/src/App.js
@@ -3,9 +3,16 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  // NEVER GOING TO...
+  // count = 123
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Add Animal</button>
+      <button onClick={handleClick}>Add Animal</button>
       <div>Number of animals: {count}</div>
     </div>
   );
