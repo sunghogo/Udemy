ea2f52ef0e943786672cd3214f3d53872a84a661
sunghogo <leinad5592@gmail.com>
2023-06-06 10:43:35 -0400

    MRR2023: Section 5: 75. Reminder on Async:Await: Made the submit handler an async function to properly await image fetching

--- Modern React with Redux [2023 Update]/Lecture Examples/pics/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/pics/src/App.js
@@ -2,8 +2,8 @@ import SearchBar from "./components/SearchBar";
 import searchImages from "./api";
 
 function App() {
-  const handleSubmit = (term) => {
-    searchImages(term);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
   };
 
   return (
