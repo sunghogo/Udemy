07e790f2aa08bb1af776f015176f266b81497d68
sunghogo <leinad5592@gmail.com>
2023-06-06 09:33:00 -0400

    MRR2023: Section 5: 66. Data Fetching Cleanups: Cleaning up api.js module

--- Modern React with Redux [2023 Update]/Lecture Examples/pics/src/api.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/pics/src/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID OnNKq6e_OhYd6YAxu5CtYwNteGGBQbfEU6qXecIaYBs",
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
--- Modern React with Redux [2023 Update]/Lecture Examples/pics/src/index.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/pics/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import searchImages from "./api";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
