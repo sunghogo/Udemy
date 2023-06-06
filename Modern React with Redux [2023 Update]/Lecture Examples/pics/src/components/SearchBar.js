4ec571259259efc9fe6099d7485cefda9bb4cbba
sunghogo <leinad5592@gmail.com>
2023-06-06 10:01:00 -0400

    MRR2023: Section 5: L70 Handling Form Submission: Wrapped SearchBar input element inside a form and disabled default behavior to handle form submission

--- Modern React with Redux [2023 Update]/Lecture Examples/pics/src/components/SearchBar.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/pics/src/components/SearchBar.js
@@ -1,12 +1,15 @@
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
     onSubmit("cars");
   };
 
   return (
     <div>
-      <input></input>
-      <button onClick={handleClick}>Click me</button>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
     </div>
   );
 }
