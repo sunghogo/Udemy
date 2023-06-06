2bdbbf3fb34f1fb0cb075991bca7a95d6e9bc055
sunghogo <leinad5592@gmail.com>
2023-06-06 12:45:48 -0400

    MRR2023: Section 6: 104. Adding a Book, For Real!: createBook event handler now properly updates books state with passed title

--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/App.js
@@ -5,13 +5,15 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Entered book title: ", title);
+    const updatedBooks = [...books, { id: 123, title }];
+    setBooks(updatedBooks);
   };
   const handleBook = () => {};
   const deleteBook = () => {};
 
   return (
     <div>
+      {books.map((e) => e.title).join(" ")}
       <BookCreate onCreate={createBook} />
     </div>
   );
