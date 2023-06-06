3af999ddf9b5fc9a2945e9184b0ccad168697283
sunghogo <leinad5592@gmail.com>
2023-06-06 11:52:15 -0400

    MRR2023: Section 6: 86. Reminder on Event Handlers: Set event callbacks boilerplate

--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/App.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const createBook = () => {};
+  const handleBook = () => {};
+  const deleteBook = () => {};
+
   return <div>lmao</div>;
 }
 
