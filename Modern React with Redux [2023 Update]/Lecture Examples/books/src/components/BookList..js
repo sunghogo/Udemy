de195a76857fbdd0e5c15f6030e60377f4cf3ebc
sunghogo <leinad5592@gmail.com>
2023-06-07 11:06:11 -0400

    MRR2023: Section 7: 131. Context in Action: Creating and using BooksContext

--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookList..js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookList..js
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
+  console.log(value);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
