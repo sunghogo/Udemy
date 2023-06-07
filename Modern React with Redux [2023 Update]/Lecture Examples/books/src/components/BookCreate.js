5c0b44542f7ac11185157d01e625cd97218c7ea0
sunghogo <leinad5592@gmail.com>
2023-06-07 12:09:25 -0400

    MRR2023: Section 8: 141. A Small Taste of Reusable Hooks: Refactored app to use custom useBooksContext hook

--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookCreate.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookCreate.js
@@ -1,9 +1,9 @@
-import { useState, useContext } from "react";
-import BooksContext from "../context/books";
+import { useState } from "react";
+import useBooksContext from "../hooks/useBooksContext";
 
 function BookCreate() {
   const [title, setTitle] = useState("");
-  const { createBook } = useContext(BooksContext);
+  const { createBook } = useBooksContext();
 
   const handleChange = (e) => {
     setTitle(e.target.value);
