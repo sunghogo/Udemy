7295ebe2393f2849da3dc138e5b1ba9a8b1b9da3
sunghogo <leinad5592@gmail.com>
2023-06-07 11:54:21 -0400

    MRR2023: Section 7: 140. Last Bit of Refactoring: Refactor app to use BooksContext to propagate down books state and its RESTful methods

--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/App.js
@@ -1,56 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList.";
-import axios from "axios";
+import BooksContext from "./context/books";
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-
-    setBooks(response.data);
-  };
-
+  const { fetchBooks } = useContext(BooksContext);
   // Second argument: [] = only first render, No argument = after every 1st/rerender, ['variables'] = after every 1st/rerender if one of the 'variables' changed
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const createBook = async (title) => {
-    if (title === "") return;
-
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
-
-    const updatedBooks = books.filter((book) => book.id !== id);
-    setBooks(updatedBooks);
-  };
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    const updatedBooks = books.map((book) =>
-      book.id === id ? { ...book, ...response.data } : book
-    );
-    setBooks(updatedBooks);
-  };
-
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookCreate.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookCreate.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
-function BookCreate({ onCreate }) {
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
+
+function BookCreate() {
   const [title, setTitle] = useState("");
+  const { createBook } = useContext(BooksContext);
 
   const handleChange = (e) => {
     setTitle(e.target.value);
@@ -8,7 +11,7 @@ function BookCreate({ onCreate }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onCreate(title);
+    createBook(title);
     setTitle("");
   };
 
