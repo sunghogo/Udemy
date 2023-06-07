d083ce61499b0e1bb5e911ea6cc5f2c1fe526882
sunghogo <leinad5592@gmail.com>
2023-06-07 11:37:07 -0400

    MRR2023: Section 7: 135. Refactoring to Use Context: Removed test state from BooksContext

--- Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookList..js
+++ Modern React with Redux [2023 Update]/Lecture Examples/books/src/components/BookList..js
@@ -3,21 +3,13 @@ import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext);
-
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     );
   });
 
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>click</button>
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
