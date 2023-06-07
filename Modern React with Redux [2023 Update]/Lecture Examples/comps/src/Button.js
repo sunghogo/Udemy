3a076473a3c772673f644001d9fcd726c7c8326d
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-07 18:09:45 -0400

    MRR2023: Section 10: 164. The ClassNames Library: Installed and imported classnames library

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/Button.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/Button.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import className from "classnames";
 
 // Wrapper
 function Button({
@@ -11,6 +12,19 @@ function Button({
   outline,
   rounded,
 }) {
+  const finalClassName = className("px-3", "py-1.5", {
+    "bg-blue-500": primary,
+    "border-blue-700": primary,
+    // "bg-blue-500": secondary,
+    // "border-blue-700": secondary,
+    // "bg-blue-500": success,
+    // "border-blue-700": success,
+    // "bg-blue-500": warning,
+    // "border-blue-700": warning,
+    // "bg-blue-500": danger,
+    // "border-blue-700": danger,
+  });
+
   // Underlying/Wrapper Element
   return (
     <button className="px-3 py-1.5 border border-blue-700 bg-blue-500 text-white">
