210c0f7622ac59335980fe91e2aefef87a4a64ae
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-07 18:20:26 -0400

    MRR2023: Section 10: 166. Finalizing the Variations: Finished Button component styling for rounded and outline

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/Button.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/Button.js
@@ -12,12 +12,20 @@ function Button({
   outline,
   rounded,
 }) {
+  // Later classNames styling will take priority for conflicting styles
   const classes = className("px-3 py-1.5 border", {
     "border-blue-500 bg-blue-500 text-white": primary,
     "border-gray-900 bg-gray-900 text-white": secondary,
     "border-green-500 bg-green-500 text-white": success,
     "border-yellow-400 bg-yellow-400 text-white": warning,
     "border-red-500 bg-red-500 text-white": danger,
+    "rounded-full": rounded,
+    "bg-white": outline,
+    "text-blue-500": outline && primary,
+    "text-gray-900": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-500": outline && warning,
+    "text-red-500": outline && danger,
   });
 
   // Underlying/Wrapper Element
