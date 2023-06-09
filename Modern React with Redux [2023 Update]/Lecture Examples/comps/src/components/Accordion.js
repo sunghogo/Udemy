da2010b0654138ccf5b0751b8120faf1b6bb752b
sunghogo <leinad5592@gmail.com>
2023-06-09 10:24:53 -0400

    MRR2023: Section 11: 181. Inline Event Handlers: Added expand event handler to Accordion item label

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Accordion.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Accordion.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState("");
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div
+          onClick={() =>
+            isExpanded ? setExpandedIndex("") : setExpandedIndex(index)
+          }
+        >
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
