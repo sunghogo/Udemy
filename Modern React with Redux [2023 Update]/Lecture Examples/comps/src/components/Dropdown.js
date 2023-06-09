5c3ec853a12768519c69e1e2e8fe4d99d9179fe0
sunghogo <leinad5592@gmail.com>
2023-06-09 12:22:38 -0400

    MRR2023: Section 12: 202. Adding Styling: Added Tailwind styling to Dropdown component

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Dropdown.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,16 +15,29 @@ function Dropdown({ options, value, onChange }) {
 
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} onClick={() => handleOptionClick(option)}>
+      <div
+        className="hover:bg-sky-100 round cursor-pointer p-1"
+        key={option.value}
+        onClick={() => handleOptionClick(option)}
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || `Select...`}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative ">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || `Select...`} <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
