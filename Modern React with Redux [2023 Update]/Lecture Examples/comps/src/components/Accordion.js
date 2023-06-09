4a9e08d3fc4202707d2324eed178cd05a2160450
sunghogo <leinad5592@gmail.com>
2023-06-09 10:34:26 -0400

    MRR2023: Section 11: 184. Displaying Icons: Added actual icons

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Accordion.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState("");
@@ -9,7 +10,9 @@ function Accordion({ items }) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    const icon = <span>{isExpanded ? "DOWN" : "LEFT"}</span>;
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
 
     return (
       <div key={item.id}>
