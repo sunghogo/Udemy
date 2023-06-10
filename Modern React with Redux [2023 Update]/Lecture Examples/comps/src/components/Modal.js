b8eb6a5aa77f0a92f69911539a42004d92d09e31
sunghogo <leinad5592@gmail.com>
2023-06-10 12:00:14 -0400

    MRR2023: Section 14: 239. Customizing the Modal: Adding button and children text to modal

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Modal.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Modal.js
@@ -1,13 +1,16 @@
 import ReactDOM from "react-dom";
 
-function Modal({ onClose }) {
+function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
       <div
         className="absolute inset-0 bg-gray-300 opacity-80"
         onClick={onClose}
       ></div>
-      <div className="absolute inset-40 pg-10 bg-white">I'm a modal</div>
+      <div className="absolute inset-40 pg-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
     </div>,
     document.querySelector(".modal-container")
   );
