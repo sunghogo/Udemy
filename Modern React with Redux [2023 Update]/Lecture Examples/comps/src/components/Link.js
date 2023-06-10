46879c052f0f6698b60f50f4c57e22dfa657d090
sunghogo <leinad5592@gmail.com>
2023-06-10 11:13:25 -0400

    MRR2023: Section 13: 231. Highlighting the Active Link: Added styling to highlight active links

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Link.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Link.js
@@ -1,10 +1,14 @@
 import classNames from "classnames";
 import useNavigation from "../hooks/useNavigation";
 
-function Link({ to, children }) {
-  const { navigate } = useNavigation();
+function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
 
-  const classes = classNames("text-blue-500");
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    currentPath === to && activeClassName
+  );
 
   const handleClick = (event) => {
     // Shortcircuit handler and resume normal behavior in new tab
