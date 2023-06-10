1973b7a0a3769b6f35bbe687048383aafc0f5d02
sunghogo <leinad5592@gmail.com>
2023-06-10 10:29:04 -0400

    MRR2023: Section 13: 225. A Link Component: Created Link component to override user navigation

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/App.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/App.js
@@ -1,5 +1,12 @@
+import Link from "./components/Link";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Link to="/accordion">Go to accordion</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
+    </div>
+  );
 }
 
 export default App;
