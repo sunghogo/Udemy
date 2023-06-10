43b56a7425e16256d794dc06b718f67d3bc7a5d1
sunghogo <leinad5592@gmail.com>
2023-06-10 10:45:34 -0400

    MRR2023: Section 13: 227. Handling Control and Command Keys: Link component now handles ctrl/meta key click

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Link.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Link.js
@@ -5,11 +5,18 @@ function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (event) => {
+    // Shortcircuit handler and resume normal behavior in new tab
+    if (event.metaKey || event.ctrlKey) return;
+
     event.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
