c519978572073b673c5dec40a871b7dc6f7829ef
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-11 10:44:18 -0400

    MRR2023: Section 15: 247. Better Styling: Added styling to table

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/TablePage.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/TablePage.js
@@ -4,7 +4,7 @@ function TablePage() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
     { name: "Apple", color: "bg-red-500", score: 3 },
-    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Banana", color: "bg-yellow-300", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
