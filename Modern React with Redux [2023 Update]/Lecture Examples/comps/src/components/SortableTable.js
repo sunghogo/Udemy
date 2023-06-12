91aae0860f50a886d53661ef2a5b363e54c7eea3
sunghogo <leinad5592@gmail.com>
2023-06-12 16:42:10 -0400

    MRR2023: Section 16: 268. Watching for Header Cell Clicks: Added click handlers on sortable headers

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/SortableTable.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/SortableTable.js
@@ -3,10 +3,21 @@ import Table from "./Table";
 function SortableTable(props) {
   const { config } = props;
 
+  const handleClick = (label) => {
+    console.log(label);
+  };
+
   const updatedConfig = config.map((column) => {
     return !column.sortValue
       ? column
-      : { ...column, header: () => <th>{column.label} IS SORTABLE</th> };
+      : {
+          ...column,
+          header: () => (
+            <th onClick={() => handleClick(column.label)}>
+              {column.label} IS SORTABLE
+            </th>
+          ),
+        };
   });
 
   return <Table {...props} config={updatedConfig} />;
