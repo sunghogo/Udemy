d298c778b09cea1b59687f9b880d75176efbe4fc
sunghogo <leinad5592@gmail.com>
2023-06-12 17:54:02 -0400

    MRR2023: Section 18: 284. Adding the Form: Refactored CounterPage to use state, implementet decrement button and add form with input and button

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/CounterPage.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/CounterPage.js
@@ -1,14 +1,39 @@
+import { useState, useEffect } from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/useCounter";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        ></input>
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 
