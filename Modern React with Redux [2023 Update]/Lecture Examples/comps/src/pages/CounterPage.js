97b4460408b09523a91067faf8b75b60a320fa55
sunghogo <leinad5592@gmail.com>
2023-06-12 18:56:07 -0400

    MRR2023: Section 18: 294. Immer in Action: Refactured reducer to use Immer library

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/CounterPage.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/CounterPage.js
@@ -1,6 +1,7 @@
 import { useState, useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
+import { produce } from "immer";
 
 const INCREMENT_COUNT = "increment-count";
 const DECREMENT_COUNT = "decrement-count";
@@ -10,23 +11,33 @@ const ADD_VALUE_TO_COUNT = "add-value-to-cout";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return { ...state, count: state.count + 1 };
+      // return { ...state, count: state.count + 1 };
+      state.count += 1;
+      return;
     case DECREMENT_COUNT:
-      return { ...state, count: state.count - 1 };
+      // return { ...state, count: state.count - 1 };
+      state.count -= 1;
+      return;
     case SET_VALUE_TO_ADD:
-      return { ...state, valueToAdd: action.payload };
+      // return { ...state, valueToAdd: action.payload };
+      state.valueToAdd = action.payload;
+      return;
     case ADD_VALUE_TO_COUNT:
-      return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+      // return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+      state.count += state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
     default:
       // throw new Error(`unexpected action type: ${action.type}`);
-      return state;
+      // return state;
+      return;
   }
 };
 
 function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount);
   // const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
