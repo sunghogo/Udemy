3b438f8ae89407688213e8cc678b826f291b9586
sunghogo <leinad5592@gmail.com>
2023-06-12 17:01:28 -0400

    MRR2023: Section 16: 271. Yessssss, It Worked!: Implemented sorting ascending/descending by column

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/SortableTable.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/SortableTable.js
@@ -5,7 +5,7 @@ function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
 
-  const { config } = props;
+  const { config, data } = props;
 
   const handleClick = (label) => {
     if (sortOrder === null) {
@@ -33,7 +33,23 @@ function SortableTable(props) {
         };
   });
 
-  return <Table {...props} config={updatedConfig} />;
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => sortBy === column.label);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
 
 export default SortableTable;
