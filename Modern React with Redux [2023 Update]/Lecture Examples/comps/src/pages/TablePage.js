e84bfcc0fa773ed17cb719618294c4b98ec98b49
sunghogo <leinad5592@gmail.com>
2023-06-12 17:21:26 -0400

    MRR2023: Section 16: 275. Table Wrapup: TablePage, Table, and SortableTable are now resuable

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/TablePage.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/TablePage.js
@@ -6,6 +6,7 @@ function TablePage() {
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-300", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Cherry", color: "bg-red-700", score: 2.5 },
   ];
 
   const config = [
@@ -22,6 +23,11 @@ function TablePage() {
       label: "Score",
       render: (fruit) => fruit.score,
       sortValue: (fruit) => fruit.score,
+    },
+    {
+      label: "Score Squared",
+      render: (fruit) => fruit.score ** 2,
+      sortValue: (fruit) => fruit.score ** 2,
     },
   ];
 
