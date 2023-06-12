ec01b083eed5eb7bdfd9135af888fdec96995b6f
sunghogo <leinad5592@gmail.com>
2023-06-12 16:53:19 -0400

    MRR2023: Section 16: 270. Adding Sort State: Implemented sortOrder and sortBy states

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/SortableTable.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/SortableTable.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
   const { config } = props;
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
