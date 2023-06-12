8080521e08e827e76075462992c167c0a73f6299
sunghogo <leinad5592@gmail.com>
2023-06-12 18:31:04 -0400

    MRR2023: Section 18: 289. Constant Action Types: Declaring action types at top similar to header variables to prevent typo comparison bugs

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/CounterPage.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/comps/src/pages/CounterPage.js
@@ -2,12 +2,15 @@ import { useState, useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment-count";
+const SET_VALUE_TO_ADD = "change-value-to-add";
+
 const reducer = (state, action) => {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT_COUNT) {
     return { ...state, count: state.count + 1 };
   }
 
-  if (action.type === "change-value-to-add") {
+  if (action.type === SET_VALUE_TO_ADD) {
     return { ...state, valueToAdd: action.payload };
   }
 
@@ -24,7 +27,7 @@ function CounterPage({ initialCount }) {
 
   const increment = () => {
     dispatch({
-      type: "increment",
+      type: INCREMENT_COUNT,
     });
   };
 
@@ -38,7 +41,7 @@ function CounterPage({ initialCount }) {
     const value = parseInt(event.target.value) || 0;
 
     dispatch({
-      type: "change-value-to-add",
+      type: SET_VALUE_TO_ADD,
       payload: value,
     });
     // setValueToAdd(value);
