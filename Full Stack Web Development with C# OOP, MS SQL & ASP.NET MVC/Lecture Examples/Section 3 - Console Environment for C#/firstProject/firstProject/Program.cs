76e30062d8bc727429df3febb9d44851ead4ea44
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-18 11:08:21 -0400

    FSWD: Section 3: 10. Functions Used in Console Environment to Write and Read

---
+++ Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/Section 3 - Console Environment for C#/firstProject/firstProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ilk_uygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ");
+            Console.WriteLine("What is your name");
+            Console.ReadKey();
+        }
+    }
+}
+
