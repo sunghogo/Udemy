6f8bbcaab8daf29bbb91ae2fbc07448da1959a45
sunghogo <leinad5592@gmail.com>
2023-06-19 11:42:50 -0400

    FSWD: Section 6: 17. Using MessageBox

--- Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/secondProject/Form1.cs
+++ Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/secondProject/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace secondProject
 {
@@ -17,6 +18,40 @@ namespace secondProject
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 17. Using MessageBox
+            //string messageText = "Simple message text";
+            //string messageTitle = "Message title";
+            //MessageBox.Show(messageText, messageTitle);
+
+            {
+                string message = "Did you see icon?";
+                string title = "ok";
+                MessageBox.Show(
+                    message,
+                    title,
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Information
+                );
+            }
+
+            {
+                string message = "Do you want to close the window";
+                string messageTitle = "Close Window";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show(message, messageTitle, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    Debug.WriteLine("Hi Debug Message Below");
+                    Debug.WriteLine(this);
+                    this.Close();
+                }
+                else if (result == DialogResult.No)
+                {
+                    // do nothing
+                }
+            }
+        }
     }
 }
