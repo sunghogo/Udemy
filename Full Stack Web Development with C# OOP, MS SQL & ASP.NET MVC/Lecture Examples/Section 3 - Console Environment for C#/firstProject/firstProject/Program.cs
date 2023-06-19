f03fc155da88eae99fd85673ba8e20709626b535
sunghogo <leinad5592@gmail.com>
2023-06-19 10:13:53 -0400

    FSWD: Section 3: 11. Read & Readline

--- Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/Section 3 - Console Environment for C#/firstProject/firstProject/Program.cs
+++ Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/Section 3 - Console Environment for C#/firstProject/firstProject/Program.cs
@@ -5,10 +5,21 @@ namespace ilk_uygulama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello ");
-            Console.WriteLine("What is your name");
+            // 10.Functions Used in Console Environment to Write and Read
+            //Console.WriteLine("Hello ");
+            //Console.WriteLine("What is your name");
+            //Console.ReadKey();
+
+            // 11. Read & Readline
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + " welcome to our lesson");
+            Console.ReadKey();
+
+            Console.WriteLine("What is your name again?");
+            int asciiCode = Console.Read();
+            Console.WriteLine(asciiCode);neeo
             Console.ReadKey();
         }
     }
 }
-
