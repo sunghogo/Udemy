8d4e1fbdadc4924a37034dfe999cb6c9b5d65dc2
sunghogo <leinad5592@gmail.com>
2023-06-19 10:44:40 -0400

    FSWD: Section 4: 13.Type Conversions in Variables

--- Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/Section 4 - Variables in C#/Variables/Program.cs
+++ Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/Section 4 - Variables in C#/Variables/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.VisualBasic;
+using System.Runtime.CompilerServices;
+
 namespace Variables
 {
     class Program
@@ -17,6 +20,43 @@ namespace Variables
             int edge1 = 10,
                 edge2,
                 edge3;
+
+            //13.Type Conversions in Variables
+            {
+                byte x = 12;
+                short y = 13;
+                int z = 4;
+                long t = 5;
+                float k = 6;
+
+                y = x;
+                z = y;
+
+                // Error, cannot implicit type convert from larger to smaller types
+                //x = y;
+                //y = z;
+            }
+
+            {
+                double x = 24;
+                int y;
+                y = (int)x;
+                y = Convert.ToInt32(x);
+            }
+
+            {
+                double x = 24;
+                int y = 12;
+                float z = 6;
+                char t = '+';
+                string s = x.ToString();
+                s = z.ToString();
+                s = t.ToString();
+                s = y.ToString();
+
+                Console.WriteLine(s);
+                Console.ReadKey();
+            }
         }
     }
 }
