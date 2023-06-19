f22e4812ba985d4a786a8f59a815b47e18fcf5d0
sunghogo <leinad5592@gmail.com>
2023-06-19 10:30:26 -0400

    FSWD: Section 4: 12. What Is Variables in C# ?

---
+++ Full Stack Web Development with C# OOP, MS SQL & ASP.NET MVC/Lecture Examples/Section 4 - Variables in C#/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //12. What Is Variables in C# ?
+            string name;
+            float shoe_size;
+
+            string school_name;
+
+            string name1,
+                name2;
+            int edge1 = 10,
+                edge2,
+                edge3;
+        }
+    }
+}
