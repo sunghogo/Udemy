5621a38a1d76320c1fffd0b7fdd08ade7322bb16
sunghogo <leinad5592@gmail.com>
2023-06-23 10:08:48 -0400

    MRR2023: Section 20: 325. Maintaining a Collection with a Slice: Implemented cars slice

---
+++ Modern React with Redux [2023 Update]/Lecture Examples/cars/src/store/slices/carsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reactjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      // Assumption:
+      // action.payload === {name: 'car', cost: 140}
+      state.cars.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      // Assumption:
+      // action.payload === the id of the car we want to remove
+      const updated = state.cars.filter((car) => car.id !== action.payload);
+      state.cars = updated;
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
