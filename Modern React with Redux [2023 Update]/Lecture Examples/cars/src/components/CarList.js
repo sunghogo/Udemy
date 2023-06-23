4a550b1c52a0900946f9c521f93947a6a8f2c320
sunghogo <leinad5592@gmail.com>
2023-06-23 11:16:07 -0400

    MRR2023: Section 20: 332. Deleting Records: Implemented CarList component car delete functionality

--- Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarList.js
@@ -1,12 +1,13 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCar } from "../store";
 
 function CarList() {
-  const cars = useSelector((state) => {
-    return state.cars.data;
-  });
+  const dispatch = useDispatch();
+
+  const cars = useSelector((state) => state.cars.data);
 
   const handleCarDelete = (car) => {
-    //..
+    dispatch(removeCar(car.id));
   };
 
   const renderedCars = cars.map((car) => {
@@ -15,7 +16,10 @@ function CarList() {
         <p>
           {car.name} - ${car.cost}
         </p>
-        <button className="button is-danger" onClick={handleCarDelete(car)}>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
           Delete
         </button>
       </div>
