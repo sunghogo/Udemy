7bfe141425d007cecf241223c238c98ba33067d7
sunghogo <leinad5592@gmail.com>
2023-06-23 11:08:02 -0400

    MRR2023: Section 20: 331. Listing the Records: Implemented CarList component displaying rendered cars

--- Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarList.js
@@ -5,7 +5,29 @@ function CarList() {
     return state.cars.data;
   });
 
-  return <div>CarList</div>;
+  const handleCarDelete = (car) => {
+    //..
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button className="button is-danger" onClick={handleCarDelete(car)}>
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
