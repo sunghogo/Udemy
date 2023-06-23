870a97b7d6f41a79298a1bef4c4d735b017b53c8
sunghogo <leinad5592@gmail.com>
2023-06-23 11:57:43 -0400

    MRR2023: Section 20: 340. Highlighting Existing Cars: form now highlights matching cars in CarsList componentsd

--- Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarList.js
@@ -5,19 +5,27 @@ function CarList() {
   const dispatch = useDispatch();
 
   // FIXME: Rerender bug because useSelector returns a new object everytime
-  const cars = useSelector(({ cars: { data, searchTerm } }) =>
-    data.filter((car) =>
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
       car.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  );
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
+  });
 
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
   };
 
   const renderedCars = cars.map((car) => {
+    // DECIDE IF THIS CAR SHOULD BE BOLD
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
