41b62089ad0ac63f41087243427534821d45db39
sunghogo <leinad5592@gmail.com>
2023-06-23 10:57:03 -0400

    MRR2023: Section 20: 329. Dispatching During the Form Submission: Implemented CarForm component submit button to dispatch addCar reducer

--- Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarForm.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/cars/src/components/CarForm.js
@@ -1,15 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
-import { changeName, changeCost } from "../store";
+import { changeName, changeCost, addCar } from "../store";
 
 function CarForm() {
   const dispatch = useDispatch();
-  const { name, cost } = useSelector((state) => ({
-    name: state.form.name,
-    cost: state.form.cost,
-  }));
+  // FIXME: Rerender bug because useSelector returns a new object everytime
+  //   const { name, cost } = useSelector((state) => ({
+  //     name: state.form.name,
+  //     cost: state.form.cost,
+  //   }));
 
-  // FIXME: Potential rerender bug because useSelector returns a new object everytime
-  console.log("Car Form Rerender");
+  const name = useSelector((state) => state.form.name);
+  const cost = useSelector((state) => state.form.cost);
 
   const handleNameChange = (event) => {
     dispatch(changeName(event.target.value));
@@ -20,10 +21,16 @@ function CarForm() {
     dispatch(changeCost(carCost));
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(addCar({ name, cost }));
+  };
+
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -42,6 +49,10 @@ function CarForm() {
               onChange={handleCostChange}
               type="number"
             />
+          </div>
+
+          <div className="field">
+            <button className="button is-link">Submit</button>
           </div>
         </div>
       </form>
