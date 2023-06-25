4a87157ea4193f2a2169d84c0d2c6556831ac426
sunghogo <leinad5592@gmail.com>
2023-06-25 14:36:07 -0400

    MRR2023: Section 21: 365. Local Fine-Grained Loading State: Refactoring loading state from application state in redux store to component state in UsersList component

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/UsersList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/UsersList.js
@@ -1,30 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import { Skeleton } from "./Skeleton";
 import Button from "./Button";
 
 function UsersList() {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+
   const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    // dispatch returns a promise that ALWAYS RESOLVES whether the promise is fulfilled or rejected so need to unwrap() into a new promise
+    dispatch(fetchUsers())
+      .unwrap()
+      .then(() => console.log("SUCCESS"))
+      .catch(() => console.log("FAIL"));
   }, [dispatch]);
 
   const handleUserAdd = () => {
     dispatch(addUser());
   };
 
-  console.log(addUser.pending);
-
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
