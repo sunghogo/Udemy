ca33a1518c22615b03afd47a82f3a2d5566cd64b
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-06-25 10:54:18 -0400

    MRR2023: Section 21: 355. More on Adding Thunks: Implemented extra reducer functions for dispatching thunks in users slice

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/store/thunks/fetchUsers.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/store/thunks/fetchUsers.js
@@ -2,6 +2,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Action type: users/fetch/pending OR fulfilled OR rejected
+// These action types can be accessed from a thunk function as properties: fetchUsers.pending, fetchUsers.fulfilled, fetchUsers.rejected
+// Fulfilled data is returned as the action.payload while error is returned as action.error
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
