cf5ac58b71214dacce9e2d8633326bd706fd0af5
sunghogo <leinad5592@gmail.com>
2023-06-30 11:25:13 -0400

    MRR2023: Section 22: 398. Refactoring the List: Refactored AlbumsList items into AlbumsListItem component

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/AlbumsList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/AlbumsList.js
@@ -3,6 +3,7 @@ import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import { Skeleton } from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
 
 function AlbumsList({ user }) {
   // results object returned from calling query hook has a couple properties
@@ -23,12 +24,7 @@ function AlbumsList({ user }) {
     content = <div>Error loadng albums.</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandablePanel key={album.id} header={header}>
-          List of photos in the album
-        </ExpandablePanel>
-      );
+      return <AlbumsListItem ket={album.id} album={album} />;
     });
   }
 
