ccee5bfbbd77542fa992de542a8c9a3117bb2a15
sunghogo <leinad5592@gmail.com>
2023-06-30 12:04:02 -0400

    MRR2023: Section 22: 402. More Clever Tag Implementation: Reimplemented tag design to work properly with different data availability, changed isLoading to isFetching in AlbumsList component to prevent slight pause

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/AlbumsList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/AlbumsList.js
@@ -8,7 +8,7 @@ function AlbumsList({ user }) {
   // results object returned from calling query hook has a couple properties
   // data = reponse data, error = null/Error object, isLoading = boolean if loading 1st time
   // isFetching = boolean if loading, refetch = function to rerun query
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   // mutations hook returns the actual mutation function and the results object
   const [addAlbum, results] = useAddAlbumMutation();
 
@@ -17,7 +17,7 @@ function AlbumsList({ user }) {
   };
 
   let content;
-  if (isLoading) {
+  if (isFetching) {
     content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loadng albums.</div>;
--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/store/apis/albumsApi.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/store/apis/albumsApi.js
@@ -29,7 +29,12 @@ const albumsApi = createApi({
         // Tags have to ben array containing a string or object with with type and id
         // Tags property can also be a callback that is passed result, error, arg parameter, that returns the tag array
         providesTags: (result, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          const tags = result.map((album) => {
+            return { type: "Album", id: album.id };
+          });
+          tags.push({ type: "UsersAlbums", id: user.id });
+
+          return tags;
         },
         query: (user) => {
           return {
@@ -43,7 +48,7 @@ const albumsApi = createApi({
       }),
       addAlbum: builder.mutation({
         invalidatesTags: (reult, error, user) => {
-          return [{ type: "Album", id: user.id }];
+          return [{ type: "UsersAlbums", id: user.id }];
         },
         query: (user) => {
           return {
@@ -58,7 +63,7 @@ const albumsApi = createApi({
       }),
       removeAlbum: builder.mutation({
         invalidatesTags: (reult, error, album) => {
-          return [{ type: "Album", id: album.userId }];
+          return [{ type: "Album", id: album.id }];
         },
         query: (album) => {
           return {
