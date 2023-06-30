f4f35ec019ffd57f8ac4d954ad146d768ba1a374
sunghogo <leinad5592@gmail.com>
2023-06-30 12:31:29 -0400

    MRR2023: Section 22: 406. Creating the Photo: Implemented adding photo in PhotosList component

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/PhotosList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/PhotosList.js
@@ -1,9 +1,24 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from "./Button";
 
 function PhotosList({ album }) {
   useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
 
-  return "PhotosList";
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className="flex flex-row m-2 items-center justify-between">
+        <h3 className="text-lg font-bold">Photos in {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default PhotosList;
