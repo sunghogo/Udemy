4dccb9b6ead4ee72d54c90938898d12549155a96
sunghogo <leinad5592@gmail.com>
2023-06-30 09:51:54 -0400

    MRR2023: Section 22: 388. Rendering the List: Implemented Skeleton and ExpandablePanel components for loading albums in AlbumsList component

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/AlbumsList.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/components/AlbumsList.js
@@ -1,13 +1,36 @@
 import { GoChecklist } from "react-icons/go";
 import { useFetchAlbumsQuery } from "../store";
+import { Skeleton } from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
-  // data = reponse data, error = null/Error object, isLoading = boolean
+  // data = reponse data, error = null/Error object, isLoading = boolean if loading 1st time
+  // isFetching = boolean if loading, refetch = function to rerun query
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loadng albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
 
-  return <div>Albums for {user.name}</div>;
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
