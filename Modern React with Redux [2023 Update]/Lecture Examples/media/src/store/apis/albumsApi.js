c00488be073be3389e7a6162638fe6403485e003
sunghogo <leinad5592@gmail.com>
2023-06-30 09:25:49 -0400

    MRR2023: Section 22: 385. Creating an Endpoint: Creating albumsApi with fetchAlbums endpoint

--- Modern React with Redux [2023 Update]/Lecture Examples/media/src/store/apis/albumsApi.js
+++ Modern React with Redux [2023 Update]/Lecture Examples/media/src/store/apis/albumsApi.js
@@ -1,5 +1,27 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const albumsApi = createApi({
+  // Specify store reducer path where all the API states object will be stored
   reducerPath: "albums",
+  // fetchBaseQuery is a RTK configured version of fetch()
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  // this is where we specify the various requests and endpoint
+  endpoints(builder) {
+    return {
+      // Name of the hook used for this request: albumsApi.useFetchAlbumsQuery()
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
 });
