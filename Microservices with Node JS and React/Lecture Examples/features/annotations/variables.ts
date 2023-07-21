91c118e086553850047a2ab40b2d6fb4780e63e9
sunghogo <leinad5592@gmail.com>
2023-07-21 12:23:46 -0400

    MNJR: Section 25: 601. Fixing the 'Any' Type

--- Microservices with Node JS and React/Lecture Examples/features/annotations/variables.ts
+++ Microservices with Node JS and React/Lecture Examples/features/annotations/variables.ts
@@ -31,4 +31,5 @@ const logNumber: (i: number) => void = (i) => {
 // When to use annotations
 // 1) Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}';
-const coordinates = JSON.parse(json); // {x: 10, y : 10}
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y : 10}
