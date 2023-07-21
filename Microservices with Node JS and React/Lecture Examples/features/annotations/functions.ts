703aadf806a0bedf8caa0030130edd40718fb36d
sunghogo <leinad5592@gmail.com>
2023-07-21 12:44:36 -0400

    MNJR: Section 25: 607. Void and Never

--- Microservices with Node JS and React/Lecture Examples/features/annotations/functions.ts
+++ Microservices with Node JS and React/Lecture Examples/features/annotations/functions.ts
@@ -13,3 +13,26 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+  //   return null;
+  //   return undefined;
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+const throwError3 = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
