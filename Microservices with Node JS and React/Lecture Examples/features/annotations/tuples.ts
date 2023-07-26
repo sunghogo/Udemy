e4285af3add4e92570cf729e3014440516e581e0
sunghogo <leinad5592@gmail.com>
2023-07-26 10:37:34 -0400

    MNJR: Section 25: 609. Why Tuples?

--- Microservices with Node JS and React/Lecture Examples/features/annotations/tuples.ts
+++ Microservices with Node JS and React/Lecture Examples/features/annotations/tuples.ts
@@ -11,3 +11,10 @@ const pepsi: [string, boolean, number] = ['brown', true, 40];
 // pepsi[0] = 40;
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354]; // Difficult to understand what this information means, an object is a better choice
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
