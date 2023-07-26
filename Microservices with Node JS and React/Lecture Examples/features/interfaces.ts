250149238ce2db32886f90814f6729735505b961
sunghogo <leinad5592@gmail.com>
2023-07-26 10:58:50 -0400

    MNJR: Section 25: 612. Fixing Annotations With Interfaces

--- Microservices with Node JS and React/Lecture Examples/features/interfaces.ts
+++ Microservices with Node JS and React/Lecture Examples/features/interfaces.ts
@@ -1,14 +1,28 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
+  // broken: 1,
+  // isBroken: true,
 };
 
-const printVehicle = (vehicle: {
+let printVehicle = (vehicle: {
   name: string;
   year: number;
   broken: boolean;
 }): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken?: ${vehicle.broken}`);
+};
+
+printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken?: ${vehicle.broken}`);
