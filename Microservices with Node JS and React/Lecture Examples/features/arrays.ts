be34cb2b619b7958b6ec6862d00f4fa5f8d61b3e
sunghogo <leinad5592@gmail.com>
2023-07-26 10:15:29 -0400

    MNJR: Section 25: 604. Why Typed Arrays?

--- Microservices with Node JS and React/Lecture Examples/features/arrays.ts
+++ Microservices with Node JS and React/Lecture Examples/features/arrays.ts
@@ -4,3 +4,13 @@ const dates = [new Date(), new Date()];
 
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
 const carsByMake2: string[][] = [];
+
+// Help with inference when extracting values
+const carArr = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible  values
+// carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => car.toUpperCase());
