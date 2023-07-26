667f7e4b755b35765be103d637cb46d662693a8d
sunghogo <leinad5592@gmail.com>
2023-07-26 10:03:24 -0400

    MNJR: Section 25: 603. Arrays in TypeScript

---
+++ Microservices with Node JS and React/Lecture Examples/features/arrays.ts
@@ -0,0 +1,6 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const carMakers2: string[] = [];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+const carsByMake2: string[][] = [];
