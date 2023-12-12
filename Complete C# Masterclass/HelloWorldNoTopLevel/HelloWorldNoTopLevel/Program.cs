3256c81a1fcff0177a8561c448ebcbb0079fb821
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-12 12:39:11 -0500

    CM: S1 L7: Hello World no top-level code

---
+++ Complete C# Masterclass/HelloWorldNoTopLevel/HelloWorldNoTopLevel/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorldNoTopLevel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine("I beeped");
+            Console.Beep();
+        }
+    }
+}
