1473505320396934d3b7f96140be0a8d89dc02a9
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-13 15:16:03 -0500

    CM: S2 L29: Parsing Strings

---
+++ Complete C# Masterclass/StringParsing/StringParsing/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StringParsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This only concatenates two strings
+            string myString = "15";
+            string mySecondString = "13";
+            string result = myString + mySecondString;
+            Console.WriteLine($"result: {result}");
+
+            // Parse strings first
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            Console.WriteLine($"resultInt: {resultInt}");
+
+            // Parse vs TryParse
+            myString = "15a";
+            int num1TryParse;
+            Int32.TryParse(myString, out num1TryParse);
+            Console.WriteLine($"num1TryParse: {num1TryParse}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
