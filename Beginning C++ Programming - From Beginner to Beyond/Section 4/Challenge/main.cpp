ae01036a242e6dce418ae5e67caad6099a143752
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-22 16:48:19 -0500

    CPPBB: S4 L34-35: Challenge

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 4/Challenge/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int favoriteNumber;
+
+    std::cout << "Enter your favorite number between 1 and 100: ";
+
+    std::cin >> favoriteNumber;
+
+    //std::format() is not recognized even with -std=c++20
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl << "No really!!, " << favoriteNumber << " is my favorite number!" << std::endl;
+}
