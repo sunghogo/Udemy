ab8752f91034da0b2eae4e13ad059c104fd08454
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-22 23:10:27 -0500

    CPPPBB: S5: Exercise 1 update

--- Beginning C++ Programming - From Beginner to Beyond/Section 5/exercise1.cpp
+++ Beginning C++ Programming - From Beginner to Beyond/Section 5/exercise1.cpp
@@ -5,9 +5,12 @@ using namespace std;
 void say_hi() {
     //--- WRITE YOUR CODE BELOW THIS LINE----  
     cout << "Hi Frank!";
+
+    
     //--- WRITE YOUR CODE ABOVE THIS LINE----
 }
 
 int main() {
     say_hi();
+    return 0;
 }
