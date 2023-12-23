542eaa7e15fd52a54ac6812a02d2fe9006659c77
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-22 23:11:00 -0500

    CPPPBB: S5: Exercise 2

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 5/exercise2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void sallys_dogs() {
+    
+    int x {3};
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE---- 
+    cout << "Sally has " << x << " dogs.";
+
+    
+    //---- WRITE YOUR CODE ABOVE THIS LINE----
+}
+
+int main() {
+    sallys_dogs();
+    return 0;
+}
