f36f588f0d59307c7dbda8e3aacc44bd11d2d4f6
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-24 15:54:03 -0500

    CPPPBB: Section 7: Exercise 5

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 7/exercise5.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+vector<int> use_array() {
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    int arr [10] {0};
+    arr[0] = 100;
+    arr[9] = 1000; 
+    
+    
+    
+    //-----WRITE YOUR ABOVE THIS LINE----
+    //-----DO NOT CHANGE THE CODE BELOW THIS LINE----
+    vector<int> v(arr, arr + sizeof arr / sizeof arr[0]);
+    return v;
+}
+
+int main() {
+    use_array();
+    return 0;
+}
