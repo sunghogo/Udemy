029bbde240f0fecdb7be298913d349ad253c9bc9
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-24 15:28:33 -0500

    CPPPBB: Section 6 L51: Section 6 Challenge completed

--- Beginning C++ Programming - From Beginner to Beyond/Section 6/constants.cpp
+++ Beginning C++ Programming - From Beginner to Beyond/Section 6/constants.cpp
@@ -20,23 +20,29 @@ using namespace std;
 
 int main() {
     cout << "Hello, welcome to Frank's Carpet Cleaning Service" << endl;
-    cout << "\nHow mamy rooms would you liked cleaned?" << endl;
+    cout << "\nHow mamy small rooms would you liked cleaned?" << endl;
+    int numberOfSmallRooms {0};
+    cin >> numberOfSmallRooms;
 
-    int numberOfRooms {0};
-    cin >> numberOfRooms;
+    cout << "\nHow mamy large rooms would you liked cleaned?" << endl;
+    int numberOfLargeRooms {0};
+    cin >> numberOfLargeRooms;
 
-    const double pricePerRoom {32.5};
+    const double pricePerSmallRoom {25.0};
+    const double pricePerLargeRoom {35.0};
     const double salesTax {0.06};
     const int estimateExpiry {30}; //days
 
-    const double totalPreTax = pricePerRoom * numberOfRooms;
+    const double totalPreTax = pricePerSmallRoom * numberOfSmallRooms + pricePerLargeRoom * numberOfLargeRooms;
     const double totalTax = totalPreTax * salesTax;
     const double totalPostTax = totalPreTax + totalTax;
 
 
     cout << "\nEstimate for carpet cleaning service" << endl;
-    cout << "Number of rooms: " << numberOfRooms << endl;
-    cout << "Price per room: $" << pricePerRoom << endl;
+    cout << "Number of small rooms: " << numberOfSmallRooms << endl;
+    cout << "Number of large rooms: " << numberOfLargeRooms << endl;
+    cout << "Price per small room: $" << pricePerSmallRoom << endl;
+    cout << "Price per large room: $" << pricePerLargeRoom << endl;
     cout << "Cost: $" << totalPreTax << endl;
     cout << "Tax: $" << totalPreTax * salesTax << endl;
     cout << "==============================================" << endl;
