884be83672dd170436f4b9a429147a7f3de6d3a4
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-24 14:12:12 -0500

    CPPBB: Section 6 L46: Local vs Global Variables

--- Beginning C++ Programming - From Beginner to Beyond/Section 6/main.cpp
+++ Beginning C++ Programming - From Beginner to Beyond/Section 6/main.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 // This program will calculate the area of a room in square feet
 
+// int age {18}; // global variable
+
 int main() {
     // Uninitialized variables will have unknown values stored in them
 
     // Best practice to declare variables closest to where they are first used
+
+    // Local vs global variables
+    // int age {18};
 
     cout << "Enter the width of the room: ";
     int roomWidth {0};
