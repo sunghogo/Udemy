9f65ea255271c2494168e6cc7d7a1c6ad5c63921
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-24 23:45:29 -0500

    CPPPBB: Section 8 L70: Comparison Operators

--- Beginning C++ Programming - From Beginner to Beyond/Section 8/comparison_operators.cpp
+++ Beginning C++ Programming - From Beginner to Beyond/Section 8/comparison_operators.cpp
@@ -1,8 +1,9 @@
 /*
-    ==  Equal Operator
-    !=  Not Equal Operator
-
-    Compares the values of 2 expressions, and evalutes to a boolean (true/1 or false/0)
+    >   Greater than
+    >=  Greater than or equal to
+    <   Less than
+    <=  Less than or equal to
+    <=> Three-way comparison (C++20)   
 */
 
 #include <iostream>
@@ -11,44 +12,27 @@ using namespace std;
 
 int main() {
 
-    bool equalResult {false};
-    bool notEqualResult {false};
-
     int num1 {}, num2 {};
 
-    cout << boolalpha; // flag to display true/false instead of 1/0 for booleans
-
-    cout << "Enter two integers separated by a space: ";
+    cout << boolalpha;
+    cout << "Enter 2 integers separated by a space: ";
     cin >> num1 >> num2;
-    equalResult = num1 == num2;
-    notEqualResult = num1 != num2;
-    cout << "Comparison result (equals): " << equalResult << endl;
-    cout << "Comparison result (not equals): " << notEqualResult << endl;
 
-    char char1 {}, char2 {};
-    cout << "Enter two characters separated by a space: ";
-    cin >> char1 >> char2;
-    equalResult = char1 == char2;
-    notEqualResult = char1 != char2;
-    cout << "Comparison result (equals): " << equalResult << endl;
-    cout << "Comparison result (not equals): " << notEqualResult << endl;
+    cout << num1 << " > " << num2 << ": " << (num1 > num2) << endl;
+    cout << num1 << " >= " << num2 << ": " << (num1 >= num2) << endl;
+    cout << num1 << " < " << num2 << ": " << (num1 < num2) << endl;
+    cout << num1 << " <= " << num2 << ": " << (num1 <= num2) << endl;
 
-    // Be careful with floating point comparisons since they are approximations
-    double double1 {}, double2 {};
-    cout << "Enter two doubles separated by a space: ";
-    cin >> double1 >> double2;
-    equalResult = double1 == double2;
-    notEqualResult = double1 != double2;
-    cout << "Comparison result (equals): " << equalResult << endl;
-    cout << "Comparison result (not equals): " << notEqualResult << endl;
+    const int lower {10};
+    const int upper {20};
 
-    // num1 will be to a double, and the comparison will work
-    cout << "Enter an integer and a double separated by a space: ";
-    cin >> num1 >> double1;
-    equalResult = num1 == double1;
-    notEqualResult = num1 != double1;
-    cout << "Comparison result (equals): " << equalResult << endl;
-    cout << "Comparison result (not equals): " << notEqualResult << endl;
+    cout << "\nEnter an integer that is greater than " << lower << ": ";
+    cin >> num1;
+    cout << num1 << " > " << lower << " is " << (num1 > lower) << endl;
+
+    cout << "\nEnter an integer that is less than or equal to " << upper << ": ";
+    cin >> num1;
+    cout << num1 << " <= " << upper << " is " << (num1 <= upper) << endl;
 
 
     cout << endl;
