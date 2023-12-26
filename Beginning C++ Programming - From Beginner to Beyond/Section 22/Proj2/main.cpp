1ecb933994dbbcda725fad4ac6813f9c4038c202
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-26 15:57:20 -0500

    CPPPBB: S22 L277: Building and running C++ programs in VSCode on Windows

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 22/Proj2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from Proj2" << endl;
+    cout << "Enter your favorite number: ";
+    int num {};
+    cin >> num;
+    cout << "Your favorite number is " << num << endl;
+    return 0;
+}
