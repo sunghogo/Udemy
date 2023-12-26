9b2076c0752cfefd812ad9fd089d3e5eea151855
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-26 17:12:56 -0500

    CPPPBB: Section 9 L83: for Loop

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 9/for_loop.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+    For Loop:
+        Initialization: initialize int i {0} or assign int i = 0
+        Condition: use < and >, or <= and >=
+        Increment: ++i, i++ is the same
+
+    You can initialize and increment multiple variables, separated with a ',' (Right to Left Associativity, and the result of comma operator is leftmost expression)
+
+    for (;;){} is an endless loop, but omitting expressions is not best practices
+*/
+
+int main() {
+    for (int i {1}; i <= 10; i++)
+        cout << i << endl;
+
+    for (int i {1}; i <= 10; i+=2)
+        cout << i << endl;
+
+    for (int i {10}; i > 0; i--) {
+        cout << i << endl;
+    }
+    cout << "Blastoff!" << endl;
+
+    for (int i {10}; i <= 100; i+=10) {
+        if (i % 15 == 0)
+            cout << i << endl;
+    }
+
+    for (int i{1}, j{5}; i <= 5; i++, j++) {
+        cout << i << " + " << j << " = " << i + j << endl;
+    }
+
+    for (int i{1}; i <= 100; i++) {
+        cout << i << ((i % 10 == 0) ? '\n' : ' ');
+    }
+
+    // vector.size() is an unsigned integer
+    vector<int> nums {10, 20, 30, 40, 50};
+    for (unsigned i{0}; i < nums.size(); i++)
+        cout << nums.at(i) << endl;
+
+    cout << endl;
+    return 0;
+}
