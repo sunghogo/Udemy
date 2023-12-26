894c2264fd645910505c3f13a5915ca5cdc08bb1
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-26 18:13:09 -0500

    CPPPBB: S9 L87-88: continue, break, infinte loops

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 9/continue_break_infinite_loops.cpp
@@ -0,0 +1,19 @@
+/*
+    continue: no further statements in the body of loop are executed, control goes directly to beginning of next iteration
+    
+    break: no further statements in the body of loop are executed, loop is immediately terminated
+
+    infinite loops: loop whose condition expression always evaluates to true
+        - Usually a programmer error
+        - Necessary in event loop for event-driven program or Operating System
+        - for (;;) or while (true) or do {} while (true)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << endl;
+    return 0;
+}
