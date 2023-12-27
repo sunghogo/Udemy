16d9bd24e96c5ae691221c3e7e5c67bee2a7311e
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-26 23:41:40 -0500

    CPPPBB: S10 L94: Character functions

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 10/character_functions.cpp
@@ -0,0 +1,18 @@
+/*
+    #include <cctype>
+    function_name(char)
+
+    Testing characters:
+        isalpha(c)      True if c is a letter
+        isalnum(c)      True is c is a letter or digit
+        isdigit(c)      True if c is a digit
+        islower(c)      True if c is a lowercase letter
+        isprint(c)      True if c is a printable character
+        ispunct(c)      True if c is a punctuation character
+        isupper(c)      True if c is an uppercase letter
+        isspace(c)      True if c is whitespace
+
+    Converting characters:
+        tolower(c)      Returns lowercase of c, or defaults to c
+        toupper(c)      Returns uppercase of c, or defaults to c
+*/
