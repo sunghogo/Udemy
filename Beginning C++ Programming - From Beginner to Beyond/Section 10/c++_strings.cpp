40ec90b2a80731f5b77e8aead47fed03ef05e5bb
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-28 17:12:47 -0500

    CPPPBB: S10 L98: Working with C++ Strings

--- Beginning C++ Programming - From Beginner to Beyond/Section 10/c++_strings.cpp
+++ Beginning C++ Programming - From Beginner to Beyond/Section 10/c++_strings.cpp
@@ -20,7 +20,7 @@
 
     Concatenation:
         + +=
-        -   Cannot concatenate only C-style Strings (mixed is ok):
+        -   Cannot concatenate two C-style Strings (mixed is ok since C-style string will implicit coerce to string object):
             string sentence = "Hello " + "World!"; // Doesn't compile
 
     Accesing characters:
@@ -29,7 +29,7 @@
     
     Comparison Operators:
         == != > >= < <=
-        -   Cannot compare only C-Style Strings
+        -   Cannot compare two C-Style Strings
         -   Less/Greater operators compare lexcial/ASCII values of characters
 
     Methods:
@@ -46,12 +46,119 @@
 
 
 */
+
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
 int main () {
+
+    string s0;
+    string s1 {"Apple"};
+    string s2 {"Banana"};
+    string s3 {"Kiwi"};
+    string s4 {"apple"};
+    string s5 {s1}; // Copy of Apple
+    string s6 {s1, 0, 3}; // App
+    string s7(10, 'X'); // XXXXXXXXXX
+
+    cout << s0 << endl; // No garbage
+    cout << s0.length() << endl; // Empty string
+
+    // Initialization
+    cout << "\nInitialization" << "\n====================================" << endl;
+    cout << "s1 is initialized to: " << s1 << endl;
+    cout << "s2 is initialized to: " << s2 << endl;
+    cout << "s3 is initialized to: " << s3 << endl;
+    cout << "s4 is initialized to: " << s4 << endl;
+    cout << "s5 is initialized to: " << s5 << endl;
+    cout << "s6 is initialized to: " << s6 << endl;
+    cout << "s7 is initialized to: " << s7 << endl;
+
+    // Comparisons
+    cout << "\nComparison" << "\n====================================" << endl;
+    cout << boolalpha;
+    cout << s1 << " == " << s5 << ": " << (s1 == s5) << endl;
+    cout << s1 << " == " << s2 << ": " << (s1 == s2) << endl;
+    cout << s1 << " != " << s2 << ": " << (s1 != s2) << endl;
+    cout << s1 << " < " << s2 << ": " << (s1 < s2) << endl;
+    cout << s2 << " > " << s1 << ": " << (s2 > s1) << endl;
+    cout << s4 << " < " << s5 << ": " << (s4 < s5) << endl;
+    cout << s1 << " == " << "Apple" << ": " << (s1 == "Apple") << endl;
+
+    // Assignment
+    cout << "\nAssignemnt" << "\n====================================" << endl;
+    s1 = "Watermelon";
+    cout << "s1 is now: " << s1 << endl;
+    s2 = s1;
+    cout << "s2 is now: " << s2 << endl;
+
+    s3 = "Frank";
+    cout << "s3 is now: " << s3 << endl;
+    s3[0] = 'C';
+    cout << "s3 changed first letter to 'C': " << s3 << endl;
+    s3.at(0) = 'P';
+    cout << "s3 changed first letter to 'P': " << s3 << endl;
+
+    // Concatenation
+    cout << "\nConcatenation" << "\n====================================" << endl;
+    s2 = "Banana";
+    s3 = s5 + " and " + s2 + " juice";
+    cout << "s3 is now: " << s3 << endl;
+
+    // s3 = "nice" + " cold" + s1 + " juice"; // Compilation error
+
+    // for Loop
+    cout << "\nLooping" << "\n====================================" << endl;
+    s1 = "Apple";
+    for (size_t i {0}; i < s1.length(); i++) {
+        cout << s1.at(i);
+    }
+    cout << endl;
+
+    // Range-based for loop
+    for (char c: s1) {
+        cout << c;
+    }
+    cout << endl;
+
+    // Substring
+    cout << "\nSubstring" << "\n====================================" << endl;
+    s1 = "This is a test";
+    cout << s1.substr(0, 4) << endl;
+    cout << s1.substr(5, 2) << endl;
+    cout << s1.substr(10, 4) << endl;
+
+
+    // erase
+    cout << "\nErase" << "\n====================================" << endl;
+    s1 = "This is a test";
+    s1.erase(0, 5);
+    cout << "s1 is now: " << s1 << endl;
+
+    // getline
+    cout << "\ngetline" << "\n====================================" << endl;
+    string full_name {};
+    cout << "Enter your full: ";
+    getline(cin, full_name);
+    cout << "Your full name is: " << full_name << endl;
+
+    // find
+    cout << "\nfind" << "\n====================================" << endl;
+    s1 = "The secret word is Boo";
+    string word {};
+
+    cout << "Enter the word to find: ";
+    cin >> word;
+    size_t position = s1.find(word);
+    if (position != string::npos) {
+        cout << "Found " << word << " at position: " << position << endl;
+    } else {
+        cout << "Sorry, " << word << " not found" << endl;
+    }
+
 
     cout << endl;
     return 0;
