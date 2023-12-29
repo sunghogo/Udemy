c442c472b87da7a3e438438ca24c1c38ecee5202
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-12-28 19:15:28 -0500

    CPPPBB: S10: Assignment 1

---
+++ Beginning C++ Programming - From Beginner to Beyond/Section 10/assignment1.cpp
@@ -0,0 +1,128 @@
+/*
+Write a C++ program that displays a Letter Pyramid from a user-provided std::string.
+
+Prompt the user to enter a std::string and then from that string display a Letter Pyramid as follows:
+
+It's much easier to understand the Letter Pyramid given examples.
+
+If the user enters the string "ABC", then your program should display:
+
+  A                                                                                                                    
+ ABA                                                                                                                   
+ABCBA 
+
+
+If the user enters the string, "12345", then your program should display:
+
+ 
+
+    1                                                                                                                  
+   121                                                                                                                 
+  12321                                                                                                                
+ 1234321                                                                                                               
+123454321
+ 
+
+If the user enters 'ABCDEFG', then your program should display:
+
+ 
+
+      A                                                                                                                
+     ABA                                                                                                               
+    ABCBA                                                                                                              
+   ABCDCBA                                                                                                             
+  ABCDEDCBA                                                                                                            
+ ABCDEFEDCBA                                                                                                           
+ABCDEFGFEDCBA
+If the user enters 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', then your program should display:
+
+                         A
+                        ABA
+                       ABCBA
+                      ABCDCBA
+                     ABCDEDCBA
+                    ABCDEFEDCBA
+                   ABCDEFGFEDCBA
+                  ABCDEFGHGFEDCBA
+                 ABCDEFGHIHGFEDCBA
+                ABCDEFGHIJIHGFEDCBA
+               ABCDEFGHIJKJIHGFEDCBA
+              ABCDEFGHIJKLKJIHGFEDCBA
+             ABCDEFGHIJKLMLKJIHGFEDCBA
+            ABCDEFGHIJKLMNMLKJIHGFEDCBA
+           ABCDEFGHIJKLMNONMLKJIHGFEDCBA
+          ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA
+         ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA
+        ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA
+       ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA
+      ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA
+     ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA
+    ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDCBA
+   ABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIHGFEDCBA
+  ABCDEFGHIJKLMNOPQRSTUVWXWVUTSRQPONMLKJIHGFEDCBA
+ ABCDEFGHIJKLMNOPQRSTUVWXYXWVUTSRQPONMLKJIHGFEDCBA
+ABCDEFGHIJKLMNOPQRSTUVWXYZYXWVUTSRQPONMLKJIHGFEDCBA
+If the user enters "C++isFun!", then your program should display:
+
+ 
+
+        C                                                                                                              
+       C+C                                                                                                             
+      C+++C                                                                                                            
+     C++i++C                                                                                                           
+    C++isi++C                                                                                                          
+   C++isFsi++C                                                                                                         
+  C++isFuFsi++C                                                                                                        
+ C++isFunuFsi++C                                                                                                       
+C++isFun!nuFsi++C
+
+
+Think about the problem before you begin and break it down into steps.
+
+This can be a very challenging problem to solve!
+
+*****  Don't forget to paste your code solution when you submit.******
+
+*****  Also, submit it so your peers can see it and possibly comment. ******
+
+Have fun!
+
+Questions for this assignment
+Which C++ loop(s) did you use and why?
+
+How did you handle displaying the leading spaces in each row of the pyramid?
+
+Now that you completed the assignment, how might approach the problem differently if you had to solve it again?
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "Enter letter string for Letter Pyramid: ";
+    string letters {};
+    cin >> letters;
+
+    size_t max_length = letters.length() * 2 - 1;
+    for (size_t i {0}; i < letters.length(); i++) {
+        size_t level = i + 1;
+        size_t num_letters = level * 2 - 1;
+        size_t num_spaces = max_length - num_letters;
+        string spaces(num_spaces/2, ' ');
+
+        cout << spaces;
+        
+        for (size_t j {0}; j < i; j++) {
+            cout << letters.at(j);
+        }
+        for (int j {static_cast<int>(i)}; j >= 0; j--) {
+            cout << letters.at(j);
+        }
+
+        cout << spaces << endl;
+    }
+
+    return 0;
+}
