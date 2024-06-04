f2153cc80dd5e53df9699fddc03ef1ce997f7ff4
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-06-04 11:03:04 -0400

    Deleting MRR for submodule initialization

--- Modern React with Redux [2023 Update]/Lecture Examples/comps/src/components/Route.js
+++
@@ -1,9 +0,0 @@
-import useNavigation from "../hooks/useNavigation";
-
-function Route({ path, children }) {
-  const { currentPath } = useNavigation();
-
-  return path === currentPath ? children : null;
-}
-
-export default Route;
